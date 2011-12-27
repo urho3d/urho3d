c7833616c75331972de9acdbd3324c15b3f24efe
Lasse Öörni <loorni@gmail.com>
2011-12-27 12:57:58 +0000

    Added missing files.

---
+++ Engine/Graphics/Viewport.cpp
@@ -0,0 +1,144 @@
+//
+// Urho3D Engine
+// Copyright (c) 2008-2011 Lasse Öörni
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#include "Precompiled.h"
+#include "Camera.h"
+#include "PostProcess.h"
+#include "Scene.h"
+#include "Viewport.h"
+
+Viewport::Viewport() :
+    rect_(IntRect::ZERO)
+{
+}
+
+Viewport::Viewport(Scene* scene, Camera* camera) :
+    scene_(scene),
+    camera_(camera),
+    rect_(IntRect::ZERO)
+{
+}
+
+Viewport::Viewport(Scene* scene, Camera* camera, const IntRect& rect) :
+    scene_(scene),
+    camera_(camera),
+    rect_(rect)
+{
+}
+
+Viewport::Viewport(Scene* scene, Camera* camera, const Vector<SharedPtr<PostProcess> >& postProcesses) :
+    scene_(scene),
+    camera_(camera),
+    rect_(IntRect::ZERO),
+    postProcesses_(postProcesses)
+{
+}
+
+Viewport::Viewport(Scene* scene, Camera* camera, const IntRect& rect, const Vector<SharedPtr<PostProcess> >& postProcesses) :
+    scene_(scene),
+    camera_(camera),
+    rect_(rect),
+    postProcesses_(postProcesses)
+{
+}
+
+Viewport::~Viewport()
+{
+}
+
+void Viewport::SetScene(Scene* scene)
+{
+    scene_ = scene;
+}
+
+void Viewport::SetCamera(Camera* camera)
+{
+    camera_ = camera;
+}
+
+void Viewport::SetRect(const IntRect& rect)
+{
+    rect_ = rect;
+}
+
+void Viewport::AddPostProcess(PostProcess* effect)
+{
+    if (!effect)
+        return;
+    
+    SharedPtr<PostProcess> effectPtr(effect);
+    if (!postProcesses_.Contains(effectPtr))
+        postProcesses_.Push(effectPtr);
+}
+
+void Viewport::InsertPostProcess(unsigned int index, PostProcess* effect)
+{
+    if (!effect)
+        return;
+    
+    SharedPtr<PostProcess> effectPtr(effect);
+    if (postProcesses_.Contains(effectPtr))
+        return;
+    
+    if (index >= postProcesses_.Size())
+        postProcesses_.Push(effectPtr);
+    else
+        postProcesses_.Insert(postProcesses_.Begin() + index, effectPtr);
+}
+
+void Viewport::RemovePostProcess(PostProcess* effect)
+{
+    for (Vector<SharedPtr<PostProcess> >::Iterator i = postProcesses_.Begin(); i != postProcesses_.End(); ++i)
+    {
+        if (i->Get() == effect)
+        {
+            postProcesses_.Erase(i);
+            return;
+        }
+    }
+}
+
+void Viewport::RemovePostProcess(unsigned index)
+{
+    postProcesses_.Erase(index);
+}
+
+void Viewport::RemoveAllPostProcesses()
+{
+    postProcesses_.Clear();
+}
+
+Scene* Viewport::GetScene() const
+{
+    return scene_;
+}
+
+Camera* Viewport::GetCamera() const
+{
+    return camera_;
+}
+
+PostProcess* Viewport::GetPostProcess(unsigned index) const
+{
+    return index < postProcesses_.Size() ? postProcesses_[index] : (PostProcess*)0;
+}
---
+++ Engine/Graphics/Viewport.h
@@ -0,0 +1,90 @@
+//
+// Urho3D Engine
+// Copyright (c) 2008-2011 Lasse Öörni
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#pragma once
+
+#include "Ptr.h"
+#include "Rect.h"
+
+class Camera;
+class PostProcess;
+class Scene;
+
+/// %Viewport definition either for a render surface or the backbuffer.
+class Viewport : public RefCounted
+{
+public:
+    /// Construct with defaults.
+    Viewport();
+    /// Construct with a full rectangle.
+    Viewport(Scene* scene, Camera* camera);
+    /// Construct with a specified rectangle.
+    Viewport(Scene* scene, Camera* camera, const IntRect& rect);
+    /// Construct with a full rectangle and post-processing effects.
+    Viewport(Scene* scene, Camera* camera, const Vector<SharedPtr<PostProcess> >& postProcesses);
+    /// Construct with a specified rectangle and post-processing effects.
+    Viewport(Scene* scene, Camera* camera, const IntRect& rect, const Vector<SharedPtr<PostProcess> >& postProcesses);
+    /// Destruct.
+    ~Viewport();
+    
+    /// Set scene.
+    void SetScene(Scene* scene);
+    /// Set camera.
+    void SetCamera(Camera* camera);
+    /// Set rectangle.
+    void SetRect(const IntRect& rect);
+    /// Add a post-processing effect at the end of the chain.
+    void AddPostProcess(PostProcess* effect);
+    /// Insert a post-processing effect at position in the chain.
+    void InsertPostProcess(unsigned index, PostProcess* effect);
+    /// Remove a post-processing effect.
+    void RemovePostProcess(PostProcess* effect);
+    /// Remove a post-processing effect by index.
+    void RemovePostProcess(unsigned index);
+    /// Remove all post-processing effects.
+    void RemoveAllPostProcesses();
+    
+    /// Return scene.
+    Scene* GetScene() const;
+    /// Return camera.
+    Camera* GetCamera() const;
+    /// Return rectangle.
+    const IntRect& GetRect() const { return rect_; }
+    /// Return number of post-processing effects.
+    unsigned GetNumPostProcesses() const { return postProcesses_.Size(); }
+    /// Return post-processing effect at index.
+    PostProcess* GetPostProcess(unsigned index) const;
+    /// Return all post-processing effects.
+    const Vector<SharedPtr<PostProcess> >& GetPostProcesses() const { return postProcesses_; }
+    
+private:
+    /// Scene pointer.
+    WeakPtr<Scene> scene_;
+    /// Camera pointer.
+    WeakPtr<Camera> camera_;
+    /// Viewport rectangle.
+    IntRect rect_;
+    /// Post-processing effects.
+    Vector<SharedPtr<PostProcess> > postProcesses_;
+};
+
