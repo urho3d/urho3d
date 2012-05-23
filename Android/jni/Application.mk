a8764360666be4ce4b75bf16bebc6f9def9be506
Lasse Öörni <loorni@gmail.com>
2012-05-23 23:40:53 +0000

    Compile also in armeabi-v7a mode for faster floating point operations.

--- Android/jni/Application.mk
+++ Android/jni/Application.mk
@@ -1,3 +1,4 @@
 ROOT_PATH := $(call my-dir)/../..
+APP_ABI := armeabi armeabi-v7a
 APP_STL := gnustl_static
 APP_CPPFLAGS += -fexceptions -frtti -Wno-invalid-offsetof -DUSE_OPENGL -DENABLE_PROFILING
