ddc93f6fd256aa5c0f25f3cf7ca31d14a54f2a92
Lasse Öörni <loorni@gmail.com>
2012-06-10 20:11:04 +0000

    Use orientation hint only on iOS. Do not reserve a stencil buffer on OpenGL ES.

--- Engine/Graphics/OpenGL/OGLGraphics.cpp
+++ Engine/Graphics/OpenGL/OGLGraphics.cpp
@@ -230,14 +230,21 @@ bool Graphics::SetMode(int width, int height, bool fullscreen, bool vsync, bool 
         // SDL window parameters are static, so need to operate under static lock
         MutexLock lock(GetStaticMutex());
         
+        #ifdef IOS
         SDL_SetHint(SDL_HINT_ORIENTATIONS, "LandscapeLeft LandscapeRight");
+        #endif
+        
         SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
         SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
         SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
         SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
         SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
         SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 0);
+        #ifndef GL_ES_VERSION_2_0
         SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+        #else
+        SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 0);
+        #endif
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
         
