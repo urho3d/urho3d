37a1d6683c62cc54c0e7cbd596491e8089358692
Lasse Öörni <loorni@gmail.com>
2012-07-08 22:24:58 +0000

    Increased default bias for decals.

--- Engine/Graphics/DecalSet.h
+++ Engine/Graphics/DecalSet.h
@@ -101,7 +101,7 @@ class DecalSet : public Drawable
     /// %Set maximum number of decal vertices.
     void SetMaxVertices(unsigned num);
     /// Add a decal at world coordinates, using an existing drawable's geometry for reference. Return true if successful.
-    bool AddDecal(Drawable* target, const Vector3& worldPosition, const Quaternion& worldRotation, float size, float aspectRatio, float depth, const Vector2& topLeftUV, const Vector2& bottomRightUV, float timeToLive = 0.0f, float normalCutoff = 0.25f, float depthBias = 0.0001f);
+    bool AddDecal(Drawable* target, const Vector3& worldPosition, const Quaternion& worldRotation, float size, float aspectRatio, float depth, const Vector2& topLeftUV, const Vector2& bottomRightUV, float timeToLive = 0.0f, float normalCutoff = 0.25f, float depthBias = 0.0005f);
     /// Remove n oldest decals.
     void RemoveDecals(unsigned num);
     /// Remove all decals.
