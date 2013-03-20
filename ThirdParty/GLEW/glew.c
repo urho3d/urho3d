c8ee94c9aa548ea1af8c8836032c4e6eaa574cdb
Lasse Öörni <loorni@gmail.com>
2013-03-20 22:28:16 +0000

    Fixed GLEW Linux build.

--- ThirdParty/GLEW/glew.c
+++ ThirdParty/GLEW/glew.c
@@ -29,12 +29,12 @@
 ** THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include <GL/glew.h>
+#include <glew.h>
 
 #if defined(_WIN32)
-#  include <GL/wglew.h>
+#  include <wglew.h>
 #elif !defined(__ANDROID__) && (!defined(__APPLE__) || defined(GLEW_APPLE_GLX))
-#  include <GL/glxew.h>
+#  include <glxew.h>
 #endif
 
 /*
