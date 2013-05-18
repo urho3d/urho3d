3fe13527736e2e1c4d737623558d1a8bbd28c4ce
Lasse Öörni <loorni@gmail.com>
2013-05-18 12:12:18 +0000

    Use fixed kNet build config and simplify kNet CMakeLists.txt.

---
+++ ThirdParty/kNet/include/kNetBuildConfig.h
@@ -0,0 +1,13 @@
+// This file is programmatically generated using CMake.
+// This file shows the compilation settings that were used to build kNet.
+// These need to match for the client code using kNet.
+#pragma once
+
+#ifndef KNET_ENABLE_WINXP_SUPPORT
+#define KNET_ENABLE_WINXP_SUPPORT
+#endif
+
+#ifndef KNET_NO_MAXHEAP
+#define KNET_NO_MAXHEAP
+#endif
+
