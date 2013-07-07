61a8f4a98f9a926b5ec7a5611e7de175a232cb9e
Lasse Öörni <loorni@gmail.com>
2013-07-07 23:03:20 +0000

    Fixed SDL thumb mode inline assembly when compiling in release mode with the Android toolchain.

--- cmake_android.bat
+++ cmake_android.bat
@@ -1,2 +1,2 @@
 @del /F CMakeCache.txt
-cmake -G "Unix Makefiles" -DANDROID=1 -DCMAKE_TOOLCHAIN_FILE=Android/android.toolchain.cmake -DLIBRARY_OUTPUT_PATH_ROOT=Android
+cmake -G "Unix Makefiles" -DANDROID=1 -DCMAKE_TOOLCHAIN_FILE=Android/android.toolchain.cmake -DLIBRARY_OUTPUT_PATH_ROOT=Android  -DCMAKE_BUILD_TYPE=Release
--- cmake_android.sh
+++ cmake_android.sh
@@ -1,3 +1,3 @@
 rm -f CMakeCache.txt
-cmake -G "Unix Makefiles" -DANDROID=1 -DCMAKE_TOOLCHAIN_FILE=Android/android.toolchain.cmake -DLIBRARY_OUTPUT_PATH_ROOT=Android $@
+cmake -G "Unix Makefiles" -DANDROID=1 -DCMAKE_TOOLCHAIN_FILE=Android/android.toolchain.cmake -DLIBRARY_OUTPUT_PATH_ROOT=Android -DCMAKE_BUILD_TYPE=Release $@
 sed -i.bak 's/OpenGL/Direct3D9/g' Doxyfile
