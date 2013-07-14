4f5763125cb37118a636a9706dc0a20101e4d12b
Wei Tjong Yao <weitjong@gmail.com>
2013-07-14 05:36:23 +0000

    Removed the sed command that altered the make target to install/strip as it is not needed anymore now that the stripping is done via custom command.

--- cmake_eclipse.sh
+++ cmake_eclipse.sh
@@ -4,8 +4,7 @@ cmake -E make_directory build
 rm -rf {.,build}/CMakeCache.txt CMakeFiles
 # Create the Cmake generators
 cmake -E chdir build cmake -G "Eclipse CDT4 - Unix Makefiles" .. $@
-[ $ANDROID_NDK ] && cmake -E chdir Android cmake -G "Eclipse CDT4 - Unix Makefiles" -DANDROID=1 -DCMAKE_TOOLCHAIN_FILE=android.toolchain.cmake -DLIBRARY_OUTPUT_PATH_ROOT=. .. $@ \
-                 && sed -i.bak 's/<value>all<\/value>/<value>install\/strip<\/value>/g' Android/.project
+[ $ANDROID_NDK ] && cmake -E chdir Android cmake -G "Eclipse CDT4 - Unix Makefiles" -DANDROID=1 -DCMAKE_TOOLCHAIN_FILE=android.toolchain.cmake -DLIBRARY_OUTPUT_PATH_ROOT=. .. $@
 # Assume Eclipse user uses OpenGL, comment out below sed if this is not true
 sed -i.bak 's/OpenGL/Direct3D9/g' Doxyfile
 # Create symbolic links in the build and Android directories
