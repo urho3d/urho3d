ad5bf73d10c34f6b2863ab1b31383258614bf4cf
Aster Jian <asterjian@qq.com>
2013-07-16 15:45:27 +0000

    Update Lua API. In SpriteTest.lua replace some function with property to make the code clean.

--- Bin/Data/LuaScripts/SpriteTest.lua
+++ Bin/Data/LuaScripts/SpriteTest.lua
@@ -10,7 +10,7 @@ local graphics = GetGraphics()
 local ui = GetUI()
 
 function Start()
-    if engine:IsHeadless() then
+    if engine.headless then
         ErrorDialog("SpriteTest", "Headless mode is not supported. The program will now exit.")
         engine:Exit()
         return
@@ -30,42 +30,42 @@ function InitUI()
     local uiStyle = cache:GetXMLFile("UI/DefaultStyle.xml")
     
     local debugHud = engine:CreateDebugHud()
-    debugHud:SetDefaultStyle(uiStyle)
-    debugHud:SetMode(DEBUGHUD_SHOW_ALL)
+    debugHud.defaultStyle = uiStyle
+    debugHud.mode = DEBUGHUD_SHOW_ALL
     
     local console = engine:CreateConsole()
-    console:SetDefaultStyle(uiStyle)
+    console.defaultStyle = uiStyle
     
     local cursor = Cursor:new(context)
-    cursor:SetStyleAuto(uiStyle)    
-    cursor:SetPosition(graphics:GetWidth() / 2, graphics:GetHeight() / 2)
-    ui:SetCursor(cursor)
+    cursor.styleAuto = uiStyle
+    cursor.position = IntVector2(graphics:GetWidth() / 2, graphics:GetHeight() / 2)
+    ui.cursor = cursor
     
     if GetPlatform():Eq("Android") or GetPlatform():Eq("iOS") then
-        ui:GetCursor():SetVisible(false)
+        ui.cursor.visible = false
     end
 end
 
 function InitSprites()
     local decalTex = cache:GetTexture2D("Textures/UrhoDecal.dds")
     
-    local width = graphics:GetWidth()
-    local height = graphics:GetHeight()
+    local width = graphics.width
+    local height = graphics.height
     
     for i = 1, numSprites do
         local sprite = Sprite:new(context)
-        sprite:SetTexture(decalTex)
+        sprite.texture = decalTex
         sprite:SetFullImageRect()
-        sprite:SetPosition(Random(width), Random(height))
-        sprite:SetSize(128, 128)        
-        sprite:SetHotSpot(64, 64)        
-        sprite:SetRotation(Random(360))        
-        sprite:SetScale(Random(1) + 0.5)
+        sprite.position = Vector2(Random(width), Random(height))
+        sprite:SetSize(128, 128)
+        sprite.hotSpot = IntVector2(64, 64)
+        sprite.rotation = Random(360)
+        sprite.scale = Vector2(1, 1) * (Random(1) + 0.5)
         
-        sprite:SetColor(Color(Random(0.5) + 0.5, Random(0.5) + 0.5, Random(0.5) + 0.5, 1.0))        
-        sprite:SetBlendMode(BLEND_ADD)
+        sprite:SetColor(Color(Random(0.5) + 0.5, Random(0.5) + 0.5, Random(0.5) + 0.5, 1.0))
+        sprite.blendMode = BLEND_ADD
         
-        ui:GetRoot():AddChild(sprite)
+        ui.root:AddChild(sprite)
         
         table.insert(sprites, sprite)
         table.insert(speeds, Vector2(Random(200) - 100, Random(200) - 100))        
@@ -75,16 +75,16 @@ end
 function HandleUpdate(eventType, eventData)
     local timeStep = eventData:GetFloat("TimeStep")
     
-    local width = graphics:GetWidth()
-    local height = graphics:GetHeight()
+    local width = graphics.width
+    local height = graphics.height
     
     for i = 1, numSprites do
         local sprite = sprites[i]
-        sprite:SetRotation(sprite:GetRotation() + timeStep * 30)
+        sprite.rotation = sprite.rotation + timeStep * 30
         
-        local newPos = sprite:GetPosition()
-        
+        local newPos = sprite.position
         newPos = newPos + speeds[i] * timeStep
+        
         if newPos.x >= width then
             newPos.x = newPos.x - width
         elseif newPos.x < 0 then
@@ -95,7 +95,7 @@ function HandleUpdate(eventType, eventData)
         elseif newPos.y < 0 then
             newPos.y = newPos.y + height
         end
-        sprite:SetPosition(newPos)
+        sprite.position = newPos
     end
 end
 
@@ -103,10 +103,11 @@ function HandleKeyDown(eventType, eventData)
     local key = eventData:GetInt("Key")
     
     if key == KEY_ESC then
-        if ui:GetFocusElement() == nil then
+        if ui.focusElement == nil then
             engine:Exit();
         else
-            GetConsole():SetVisible(false)
+            local console = GetConsole()
+            console.visible = false
         end
     end
     
