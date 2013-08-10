bdadbc926f26f759f645d086f9e77d642f5fffc5
Wei Tjong Yao <weitjong@gmail.com>
2013-08-10 14:03:44 +0000

    Changed VS generator to use out-of-source build.

--- cmake_vs2012.bat
+++ cmake_vs2012.bat
@@ -1,14 +1,11 @@
 @echo off
-cd Source
-del /F CMakeCache.txt
-rd /S /Q CMakeFiles
-cd ..
-del /F CMakeCache.txt
-rd /S /Q CMakeFiles
+cmake -E make_directory Build
+del /F Build\CMakeCache.txt Source\CMakeCache.txt CMakeCache.txt
+rd /S /Q Build\CMakeFiles Source\CMakeFiles CMakeFiles
 set "arch="
 for %%n in (%*) do if "%%n" == "-DENABLE_64BIT" set "arch= Win64"
 set "version=11"
 set "parm=%1"
 if "%parm:~0,9%" == "-DVERSION" set "version=%~2"
 echo on
-cmake -E chdir Source cmake -G "Visual Studio %version%%arch%" %*
+cmake -E chdir Build cmake -G "Visual Studio %version%%arch%" ../Source %*
