e2d2c0163634cc0819c3d928b17965f4987fed4c
Lasse Öörni <loorni@gmail.com>
2013-08-11 22:24:26 +0000

    Added AnimatingScene sample.

--- Source/Samples/05_AnimatingScene/AnimatingScene.cpp
+++ Source/Samples/05_AnimatingScene/AnimatingScene.cpp
@@ -31,25 +31,29 @@
 #include "Octree.h"
 #include "Renderer.h"
 #include "ResourceCache.h"
+#include "Rotator.h"
 #include "StaticModel.h"
 #include "Text.h"
 #include "UI.h"
+#include "Zone.h"
 
-#include "StaticScene.h"
+#include "AnimatingScene.h"
 
 #include "DebugNew.h"
 
 // Expands to this example's entry-point
-DEFINE_APPLICATION_MAIN(StaticScene)
+DEFINE_APPLICATION_MAIN(AnimatingScene)
 
-StaticScene::StaticScene(Context* context) :
+AnimatingScene::AnimatingScene(Context* context) :
     Sample(context),
     yaw_(0.0f),
     pitch_(0.0f)
 {
+    // Register an object factory for our custom Rotator component so that we can create them to scene nodes
+    context->RegisterFactory<Rotator>();
 }
 
-void StaticScene::Start()
+void AnimatingScene::Start()
 {
     // Execute base class startup
     Sample::Start();
@@ -67,63 +71,59 @@ void StaticScene::Start()
     SubscribeToEvents();
 }
 
-void StaticScene::CreateScene()
+void AnimatingScene::CreateScene()
 {
     ResourceCache* cache = GetSubsystem<ResourceCache>();
     
     scene_ = new Scene(context_);
     
-    // Create the Octree component to the scene. This is required before adding any drawable components, or else nothing will
-    // show up. The default octree volume will be from (-1000, -1000, -1000) to (1000, 1000, 1000) in world coordinates; it
-    // is also legal to place objects outside the volume but their visibility can then not be checked in a hierarchically
-    // optimizing manner
+    // Create the Octree component to the scene so that drawable objects can be rendered. Use default volume
+    // (-1000, -1000, -1000) to (1000, 1000, 1000)
     scene_->CreateComponent<Octree>();
     
-    // Create a child scene node (at world origin) and a StaticModel component into it. Set the StaticModel to show a simple 
-    // plane mesh with a "stone" material. Note that naming the scene nodes is optional. Scale the scene node larger
-    // (100 x 100 world units)
-    Node* planeNode = scene_->CreateChild("Plane");
-    StaticModel* planeObject = planeNode->CreateComponent<StaticModel>();
-    planeNode->SetScale(Vector3(100.0f, 1.0f, 100.0f));
-    planeObject->SetModel(cache->GetResource<Model>("Models/Plane.mdl"));
-    planeObject->SetMaterial(cache->GetResource<Material>("Materials/Stone.xml"));
+    // Create a Zone component into a child scene node. The Zone controls ambient lighting and fog settings. Like the Octree,
+    // it also defines its volume with a bounding box, but can be rotated (so it does not need to be aligned to the world X, Y
+    // and Z axes.) Drawable objects "pick up" the zone they belong to and use it when rendering; several zones can exist
+    Node* zoneNode = scene_->CreateChild("Zone");
+    Zone* zone = zoneNode->CreateComponent<Zone>();
+    // Set same volume as the Octree, set a close bluish fog and some ambient light
+    zone->SetBoundingBox(BoundingBox(-1000.0f, 1000.0f));
+    zone->SetAmbientColor(Color(0.05f, 0.1f, 0.15f));
+    zone->SetFogColor(Color(0.1f, 0.2f, 0.3f));
+    zone->SetFogStart(10.0f);
+    zone->SetFogEnd(100.0f);
     
-    // Create a directional light to the world so that we can see something. The light scene node's orientation controls the
-    // light direction; we will use the SetDirection() function which calculates the orientation from a forward direction vector.
-    // The light will use default settings (white light, no shadows)
-    Node* lightNode = scene_->CreateChild("Directional light");
-    lightNode->SetDirection(Vector3(-1.0f, -1.0f, -1.0f)); // The direction vector does not need to be normalized
-    Light* light = lightNode->CreateComponent<Light>();
-    light->SetLightType(LIGHT_DIRECTIONAL);
-    
-    // Create more StaticModel objects to the scene, randomly positioned, rotated and scaled. For rotation, we construct a
-    // quaternion from Euler angles where the Y angle (rotation about the Y axis) is randomized. The mushroom model contains
-    // LOD levels, so the StaticModel component will automatically select the LOD level according to the view distance (you'll
-    // see the model get simpler as it moves further away). Finally, rendering a large number of the same object with the
-    // same material allows instancing to be used, if the GPU supports it. This reduces the amount of CPU work in rendering the
-    // scene.
-    const unsigned NUM_OBJECTS = 200;
+    // Create randomly positioned and oriented box StaticModels in the scene
+    const unsigned NUM_OBJECTS = 2000;
     for (unsigned i = 0; i < NUM_OBJECTS; ++i)
     {
-        Node* mushroomNode = scene_->CreateChild("Mushroom");
-        StaticModel* mushroomObject = mushroomNode->CreateComponent<StaticModel>();
-        mushroomNode->SetPosition(Vector3(Random(90.0f) - 45.0f, 0.0f, Random(90.0f) - 45.0f));
-        mushroomNode->SetRotation(Quaternion(0.0f, Random(360.0f), 0.0f));
-        mushroomNode->SetScale(1.0f + Random(1.0f));
-        mushroomObject->SetModel(cache->GetResource<Model>("Models/Mushroom.mdl"));
-        mushroomObject->SetMaterial(cache->GetResource<Material>("Materials/Mushroom.xml"));
+        Node* boxNode = scene_->CreateChild("Box");
+        StaticModel* boxObject = boxNode->CreateComponent<StaticModel>();
+        boxNode->SetPosition(Vector3(Random(200.0f) - 100.0f, Random(200.0f) - 100.0f, Random(200.0f) - 100.0f));
+        // Orient using random pitch, yaw and roll Euler angles
+        boxNode->SetRotation(Quaternion(Random(360.0f), Random(360.0f), Random(360.0f)));
+        boxObject->SetModel(cache->GetResource<Model>("Models/Box.mdl"));
+        boxObject->SetMaterial(cache->GetResource<Material>("Materials/Stone.xml"));
+        // Add our custom Rotator component which will rotate the scene node each frame, when the scene sends its update event.
+        // Simply set same rotation speed for all objects
+        Rotator* rotator = boxNode->CreateComponent<Rotator>();
+        rotator->SetRotationSpeed(Vector3(10.0f, 20.0f, 30.0f));
     }
     
-    // Create a scene node for the camera, which we will move around
-    // The camera will use default settings (1000 far clip distance, 45 degrees FOV, set aspect ratio automatically)
+    // Create a scene node for the camera, which we will move around. Let the starting position be at the world origin.
+    // As the fog limits maximum visible distance, we can bring the far clip plane closer for more effective culling
+    // of distant objects
     cameraNode_ = scene_->CreateChild("Camera");
-    cameraNode_->CreateComponent<Camera>();
+    Camera* camera = cameraNode_->CreateComponent<Camera>();
+    camera->SetFarClip(100.0f);
     
-    // Set an initial position for the camera scene node above the plane
-    cameraNode_->SetPosition(Vector3(0.0f, 5.0f, 0.0f));
+    // Create a point light to the camera scene node
+    Light* light = cameraNode_->CreateComponent<Light>();
+    light->SetLightType(LIGHT_POINT);
+    light->SetRange(30.0f);
 }
 
-void StaticScene::CreateInstructions()
+void AnimatingScene::CreateInstructions()
 {
     ResourceCache* cache = GetSubsystem<ResourceCache>();
     UI* ui = GetSubsystem<UI>();
@@ -139,18 +139,16 @@ void StaticScene::CreateInstructions()
     instructionText->SetPosition(0, ui->GetRoot()->GetHeight() / 4);
 }
 
-void StaticScene::SetupViewport()
+void AnimatingScene::SetupViewport()
 {
     Renderer* renderer = GetSubsystem<Renderer>();
     
-    // Set up a viewport to the Renderer subsystem so that the 3D scene can be seen. We need to define the scene and the camera
-    // at minimum. Additionally we could configure the viewport screen size and the rendering path (eg. forward / deferred) to
-    // use, but now we just use full screen and default render path configured in the engine command line options
+    // Set up a viewport to the Renderer subsystem so that the 3D scene can be seen
     SharedPtr<Viewport> viewport(new Viewport(context_, scene_, cameraNode_->GetComponent<Camera>()));
     renderer->SetViewport(0, viewport);
 }
 
-void StaticScene::MoveCamera(float timeStep)
+void AnimatingScene::MoveCamera(float timeStep)
 {
     // Do not move if the UI has a focused element (the console)
     UI* ui = GetSubsystem<UI>();
@@ -174,8 +172,6 @@ void StaticScene::MoveCamera(float timeStep)
     cameraNode_->SetRotation(Quaternion(pitch_, yaw_, 0.0f));
     
     // Read WASD keys and move the camera scene node to the corresponding direction if they are pressed
-    // Use the TranslateRelative() function to move relative to the node's orientation. Alternatively we could
-    // multiply the desired direction with the node's orientation quaternion, and use just Translate()
     if (input->GetKeyDown('W'))
         cameraNode_->TranslateRelative(Vector3::FORWARD * MOVE_SPEED * timeStep);
     if (input->GetKeyDown('S'))
@@ -186,13 +182,13 @@ void StaticScene::MoveCamera(float timeStep)
         cameraNode_->TranslateRelative(Vector3::RIGHT * MOVE_SPEED * timeStep);
 }
 
-void StaticScene::SubscribeToEvents()
+void AnimatingScene::SubscribeToEvents()
 {
     // Subscribes HandleUpdate() method for processing update events
-    SubscribeToEvent(E_UPDATE, HANDLER(StaticScene, HandleUpdate));
+    SubscribeToEvent(E_UPDATE, HANDLER(AnimatingScene, HandleUpdate));
 }
 
-void StaticScene::HandleUpdate(StringHash eventType, VariantMap& eventData)
+void AnimatingScene::HandleUpdate(StringHash eventType, VariantMap& eventData)
 {
     // Event parameters are always defined inside a namespace corresponding to the event's name
     using namespace Update;
