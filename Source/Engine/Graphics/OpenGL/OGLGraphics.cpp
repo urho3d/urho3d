6ad8a077aa1bc137e25298c688e4a091755fa153
Lasse Öörni <loorni@gmail.com>
2013-10-04 00:06:17 +0300

    Do not place any demands on OpenGL ES 2.0 color/depth configuration to allow running on eg. RGB_565 pixel format.

--- Source/Engine/Graphics/OpenGL/OGLGraphics.cpp
+++ Source/Engine/Graphics/OpenGL/OGLGraphics.cpp
@@ -268,15 +268,13 @@ bool Graphics::SetMode(int width, int height, bool fullscreen, bool resizable, b
         #endif
 
         SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+        #ifndef GL_ES_VERSION_2_0
         SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
         SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
         SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
         SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
         SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 0);
-        #ifndef GL_ES_VERSION_2_0
         SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-        #else
-        SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 0);
         #endif
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
