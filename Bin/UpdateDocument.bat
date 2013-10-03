9d4a0ff334edf926689f6d4340eb687b28ff4797
Lasse Öörni <loorni@gmail.com>
2013-10-03 12:56:01 +0300

    Disabled the Google Code specific wiki conversion.

--- Bin/UpdateDocument.bat
+++ Bin/UpdateDocument.bat
@@ -2,8 +2,6 @@
 @echo "Dumping Script API..."
 ScriptCompiler -dumpapi ../Docs/ScriptAPI.dox
 if errorlevel 1 exit /B 1
-@echo "Converting Doxygen files to Wiki..."
-DocConverter ../Docs ../../wiki Urho3D
 if not "%1" == "-a" exit /B 0
 @echo "Converting Doxygen files to HTML..."
 cd .. && doxygen Doxyfile 1>nul
--- Bin/UpdateDocument.sh
+++ Bin/UpdateDocument.sh
@@ -2,8 +2,6 @@ cd $( dirname $0 )
 echo "Dumping Script API..."
 ./ScriptCompiler -dumpapi ../Docs/ScriptAPI.dox
 if [ $? -ne 0 ]; then exit 1; fi
-echo "Converting Doxygen files to Wiki..."
-./DocConverter ../Docs ../../wiki Urho3D
 if [ "$1" != "-a" ]; then exit 0; fi
 echo "Converting Doxygen files to HTML..."
 cd .. && doxygen Doxyfile 1>/dev/null
