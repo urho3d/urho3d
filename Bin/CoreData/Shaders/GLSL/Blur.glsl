d8983e7cff85dc97b6e557837d3be7940cc313c9
Lasse Öörni <loorni@gmail.com>
2014-02-05 18:46:59 +0200

    Converted the Blur shader.

--- Bin/CoreData/Shaders/GLSL/Blur.glsl
+++ Bin/CoreData/Shaders/GLSL/Blur.glsl
@@ -1,16 +1,29 @@
-#include "Uniforms.frag"
-#include "Samplers.frag"
-#include "PostProcess.frag"
-
-uniform vec2 cBlurDir;
-uniform float cBlurRadius;
-uniform float cBlurSigma;
-uniform vec2 cBlurHInvSize;
+#include "Uniforms.glsl"
+#include "Samplers.glsl"
+#include "Transform.glsl"
+#include "ScreenPos.glsl"
+#include "PostProcess.glsl"
 
 varying vec2 vTexCoord;
 varying vec2 vScreenPos;
 
-void main()
+#ifdef COMPILEPS
+uniform vec2 cBlurDir;
+uniform float cBlurRadius;
+uniform float cBlurSigma;
+uniform vec2 cBlurHInvSize;
+#endif
+
+void VS()
+{
+    mat4 modelMatrix = iModelMatrix;
+    vec3 worldPos = GetWorldPos(modelMatrix);
+    gl_Position = GetClipPos(worldPos);
+    vTexCoord = GetQuadTexCoord(gl_Position);
+    vScreenPos = GetScreenPosPreDiv(gl_Position);
+}
+
+void PS()
 {
     #ifdef BLUR3
         gl_FragColor = GaussianBlur(3, cBlurDir, cBlurHInvSize * cBlurRadius, cBlurSigma, sDiffMap, vTexCoord);
