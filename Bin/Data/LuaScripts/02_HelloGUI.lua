5e2571811a0fc75ae37f0bfe69eca4a415c9f670
Mike3D <e_roux@libertysurf.fr>
2014-02-10 11:57:26 +0100

    Modif2

--- Bin/Data/LuaScripts/02_HelloGUI.lua
+++ Bin/Data/LuaScripts/02_HelloGUI.lua
@@ -1,5 +1,5 @@
--- A simple 'HelloWorld' GUI created purely from code. 
--- This sample demonstrates:
+
+-- This sample demonstrates: 
 --     - Creation of controls and building a UI hierarchy
 --     - Loading UI style from XML and applying it to controls
 --     - Handling of global and per-control events
