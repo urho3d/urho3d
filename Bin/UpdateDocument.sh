163921a07283248e47192250bb20e97f5d865168
aster2013 <asterjian@qq.com>
2014-02-13 23:07:53 +0800

    UpdateDocument.bat and UpdateDocument.sh for new Lua Script Dox tool.

--- Bin/UpdateDocument.sh
+++ Bin/UpdateDocument.sh
@@ -1,23 +1,14 @@
-dumpluaapi() {
-    $CD/$1 -v 1>/dev/null 2>&1
-    if [ $? -ne 0 ]; then return 1; fi
-    echo "Dumping LuaScript API..."
-    out=`pwd`/LuaScriptAPI.dox
-    pushd ../Source/Engine/LuaScript/pkgs >/dev/null
-    $CD/$1 pkgToDox.lua $out *.pkg
-    if [ $? -ne 0 ]; then exit 1; fi
-    popd >/dev/null
-    return 0
-}
-
 cd $( dirname $0 )
 CD=`pwd`
 cd $CD/../Docs
 echo "Dumping AngelScript API..."
 ../Bin/ScriptCompiler -dumpapi ScriptAPI.dox AngelScriptAPI.h
 if [ $? -ne 0 ]; then exit 1; fi
-for l in luajit lua; do dumpluaapi $l && break; done
-if [ "$1" != "-a" ]; then exit 0; fi
+cd $CD/../Source/Engine/LuaScript/pkgs
+echo "Dumping LuaScript API..."
+../../../../Bin/tolua++ -L ToDoxHook.lua -P -o ../../../../Docs/LuaScriptAPI.dox Urho3D.tolua
+if [ $? -ne 0 ]; then exit 1; fi
+cd $CD/../Docs
 echo "Converting Doxygen files to HTML..."
 doxygen Doxyfile 1>/dev/null
 echo "Finish."
