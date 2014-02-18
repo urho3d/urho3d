5ec3c020251ca3e61ce08df453fa8492ba4b2ef6
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2014-02-18 23:22:51 +0800

    Revert back the logic to auto generate pkg list.

--- Bin/UpdateDocument.sh
+++ Bin/UpdateDocument.sh
@@ -1,14 +1,15 @@
 cd $( dirname $0 )
-CD=`pwd`
-cd $CD/../Docs
+BIN=`pwd`
+cd ../Docs
 echo "Dumping AngelScript API..."
-../Bin/ScriptCompiler -dumpapi ScriptAPI.dox AngelScriptAPI.h
+$BIN/ScriptCompiler -dumpapi ScriptAPI.dox AngelScriptAPI.h
 if [ $? -ne 0 ]; then exit 1; fi
-cd $CD/../Source/Engine/LuaScript/pkgs
 echo "Dumping LuaScript API..."
-../../../../Bin/tolua++ -L ToDoxHook.lua -P -o ../../../../Docs/LuaScriptAPI.dox Urho3D.tolua
+out=`pwd`/LuaScriptAPI.dox
+pushd ../Source/Engine/LuaScript/pkgs >/dev/null
+$BIN/tolua++ -L ToDoxHook.lua -P -o $out $BIN/LuaPkgToDox.txt
 if [ $? -ne 0 ]; then exit 1; fi
-cd $CD/../Docs
+popd >/dev/null
 echo "Converting Doxygen files to HTML..."
 doxygen Doxyfile 1>/dev/null
 echo "Finish."
