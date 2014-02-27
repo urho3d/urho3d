4a50e3d78dda0f043691c91ce64d5f6313c06582
aster2013 <asterjian@qq.com>
2014-02-27 19:04:11 +0800

    Add lua_getmainthread function to return main thread.

--- Source/ThirdParty/LuaJIT/src/lua.h
+++ Source/ThirdParty/LuaJIT/src/lua.h
@@ -110,6 +110,8 @@ typedef LUA_INTEGER lua_Integer;
 LUA_API lua_State *(lua_newstate) (lua_Alloc f, void *ud);
 LUA_API void       (lua_close) (lua_State *L);
 LUA_API lua_State *(lua_newthread) (lua_State *L);
+/* Modified by Aster Jian for Urho3D */
+LUA_API lua_State *(lua_getmainthread) (lua_State *L);
 
 LUA_API lua_CFunction (lua_atpanic) (lua_State *L, lua_CFunction panicf);
 
