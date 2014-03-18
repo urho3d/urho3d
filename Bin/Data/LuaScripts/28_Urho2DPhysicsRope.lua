2d6d3e64f5a8df647090c469ce653e15e8087a14
aster2013 <asterjian@qq.com>
2014-03-18 18:50:09 +0800

    Add Urho2DPhysicsRope Lua and AngelScript sample. [ci skip]

--- Bin/Data/LuaScripts/28_Urho2DPhysicsRope.lua
+++ Bin/Data/LuaScripts/28_Urho2DPhysicsRope.lua
@@ -1,8 +1,8 @@
--- Urho2D physics sample.
+-- Urho2D physics rope sample.
 -- This sample demonstrates:
---     - Creating both static and moving 2D physics objects to a scene
+--     - Create revolute constraint
+--     - Create roop constraint
 --     - Displaying physics debug geometry
-
 require "LuaScripts/Utilities/Sample"
 
 local scene_ = nil
@@ -39,69 +39,68 @@ function CreateScene()
     -- The camera will use default settings (1000 far clip distance, 45 degrees FOV, set aspect ratio automatically)
     cameraNode = scene_:CreateChild("Camera")
     -- Set an initial position for the camera scene node above the plane
-    cameraNode.position = Vector3(0.0, 0.0, -10.0)
+    cameraNode.position = Vector3(0.0, 5.0, -10.0)
     local camera = cameraNode:CreateComponent("Camera")
     camera.orthographic = true
 
-    local width = graphics.width * PIXEL_SIZE
-    local height = graphics.height * PIXEL_SIZE
-    camera:SetOrthoSize(Vector2(width, height))
+    local width = graphics.width
+    local height = graphics.height
+    camera:SetOrthoSize(Vector2(width, height) * 0.05)
 
     -- Create 2D physics world component
-    scene_:CreateComponent("PhysicsWorld2D")
-    
-    local boxSprite = cache:GetResource("Sprite2D", "Urho2D/Box.png")
-    local ballSprite = cache:GetResource("Sprite2D", "Urho2D/Ball.png")
+    local physicsWorld = scene_:CreateComponent("PhysicsWorld2D")
+    physicsWorld.drawJoint = true
 
     -- Create ground.
     local groundNode = scene_:CreateChild("Ground")
-    groundNode.position = Vector3(0.0, -3.0, 0.0)
-    groundNode.scale = Vector3(200.0, 1.0, 0.0)
-    
     -- Create 2D rigid body for gound
     local groundBody = groundNode:CreateComponent("RigidBody2D")
+    -- Create edge collider for ground
+    local groundShape = groundNode:CreateComponent("CollisionEdge2D")
+    groundShape:SetVertices(Vector2(-40.0, 0.0), Vector2(40.0, 0.0))
 
-    local groundSprite = groundNode:CreateComponent("StaticSprite2D")
-    groundSprite.sprite = boxSprite
+    local y = 15.0
+    local prevBody = groundBody
 
-    -- Create box collider for ground
-    local groundShape = groundNode:CreateComponent("CollisionBox2D")
-    -- Set box size
-    groundShape.size = Vector2(0.32, 0.32)
-    -- Set friction
-    groundShape.friction = 0.5
-    
-    local NUM_OBJECTS = 100
-    for i = 1, NUM_OBJECTS do
+    local NUM_OBJECTS = 10
+    for i = 0, NUM_OBJECTS - 1 do
         local node  = scene_:CreateChild("RigidBody")
-        node.position = Vector3(Random(-0.1, 0.1), 5.0 + i * 0.4, 0.0)
-
         -- Create rigid body
         local body = node:CreateComponent("RigidBody2D")
         body.bodyType = BT_DYNAMIC
 
-        local staticSprite = node:CreateComponent("StaticSprite2D")
-        local shape = nil
-        if i % 2 == 0 then
-            staticSprite.sprite = boxSprite
-            -- Create box
-            shape = node:CreateComponent("CollisionBox2D")
-            -- Set size
-            shape.size = Vector2(0.32, 0.32)
-        else
-            staticSprite.sprite = ballSprite
-            -- Create circle
-            shape = node:CreateComponent("CollisionCircle2D")
-            -- Set radius
-            shape.radius = 0.16
-        end
-        -- Set density
-        shape.density = 1.0
+        -- Create box
+        local box = node:CreateComponent("CollisionBox2D")
         -- Set friction
-        shape.friction = 0.5
-        -- Set restitution
-        shape.restitution = 0.1
+        box.friction = 0.2
+        -- Set mask bits.
+        box.maskBits = 0xFFFF - 0x0002
+
+        if i == NUM_OBJECTS - 1 then
+            node.position  = Vector3(1.0 * i, y, 0.0)
+            body.angularDamping = 0.4
+            box:SetSize(3.0, 3.0)
+            box.density = 100.0
+            box.categoryBits = 0x0002
+        else
+            node.position = Vector3(0.5 + 1.0 * i, y, 0.0)
+            box:SetSize(1.0, 0.25)
+            box.density = 20.0
+            box.categoryBits = 0x0001
+        end
+
+        local joint = node:CreateComponent("ConstraintRevolute2D")
+        joint.otherBody = prevBody
+        joint.anchorPoint = Vector2(i, y)
+        joint.collideConnected = false
+
+        prevBody = body
     end
+
+    local constraintRope = groundNode:CreateComponent("ConstraintRope2D")
+    constraintRope.otherBody = prevBody
+    constraintRope.ownerBodyAnchor = Vector2(0.0, y)
+    constraintRope.maxLength = NUM_OBJECTS - 1.0 + 0.01
 end
 
 function CreateInstructions()
@@ -171,4 +170,8 @@ function HandleUpdate(eventType, eventData)
 
     -- Move the camera, scale movement with time step
     MoveCamera(timeStep)
+
+    local physicsWorld = scene_:GetComponent("PhysicsWorld2D")
+    physicsWorld:DrawDebugGeometry();
+
 end
