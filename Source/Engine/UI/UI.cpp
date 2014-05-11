56d5cf06b6278e9a4a39258874330b08fd695b90
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2014-05-11 22:02:00 +0800

    Fix UI::Clear() erroneously also removed Console's UI elements. Closes #341.

--- Source/Engine/UI/UI.cpp
+++ Source/Engine/UI/UI.cpp
@@ -22,6 +22,7 @@
 
 #include "Precompiled.h"
 #include "CheckBox.h"
+#include "Console.h"
 #include "Context.h"
 #include "CoreEvents.h"
 #include "Cursor.h"
@@ -288,6 +289,12 @@ void UI::Clear()
     rootModalElement_->RemoveAllChildren();
     if (cursor_)
         rootElement_->AddChild(cursor_);
+    Console* console = GetSubsystem<Console>();
+    if (console)
+    {
+        rootElement_->AddChild(console->GetBackground());
+        rootElement_->AddChild(console->GetCloseButton());
+    }
 }
 
 void UI::Update(float timeStep)
