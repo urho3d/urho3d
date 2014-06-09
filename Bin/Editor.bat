6d7f211d2b61863d1001b2d15807ce2cd96f0e6d
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2014-06-09 12:45:45 +0800

    First check if doxygen is installed before attempting to update doc. [ci skip]

--- Bin/Editor.bat
+++ Bin/Editor.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 
 IF [%1] == [] (
-        "%~dp0\Urho3DPlayer" Scripts/Editor.as -w -s
+        "%~dp0Urho3DPlayer" Scripts/Editor.as -w -s
         )
 
 IF NOT [%1] == [] (
-        "%~dp0\Urho3DPlayer" Scripts/Editor.as %*
+        "%~dp0Urho3DPlayer" Scripts/Editor.as %*
         )
--- Bin/UpdateDocument.bat
+++ Bin/UpdateDocument.bat
@@ -1,4 +1,9 @@
 @echo off
+doxygen -v 1>nul 2>&1
+if %errorlevel% == 0 goto :start
+echo Could not update document because doxygen is not installed
+exit /B 1
+:start
 pushd "%~dp0..\Docs"
 echo Dumping AngelScript API...
 "%~dp0ScriptCompiler" -dumpapi ScriptAPI.dox AngelScriptAPI.h
