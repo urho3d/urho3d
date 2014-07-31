1556e887d1f07857ea99c24529a12120c4eae0e6
aster <asterjian@qq.com>
2014-07-31 22:35:20 +0800

    Add text shader.

---
+++ Bin/CoreData/Shaders/HLSL/Text.hlsl
@@ -0,0 +1,38 @@
+#include "Uniforms.hlsl"
+#include "Samplers.hlsl"
+#include "Transform.hlsl"
+
+void VS(float4 iPos : POSITION,
+        float4 iColor : COLOR0,
+        float2 iTexCoord : TEXCOORD0,
+        out float4 oPos : POSITION,
+        out float4 oColor : COLOR0,
+        out float2 oTexCoord : TEXCOORD0)
+{
+    float4x3 modelMatrix = iModelMatrix;
+    float3 worldPos = GetWorldPos(modelMatrix);
+    oPos = GetClipPos(worldPos);
+    oColor = iColor;
+    oTexCoord = iTexCoord;
+}
+
+void PS(float4 iColor : COLOR0,
+        float2 iTexCoord : TEXCOORD0,
+        out float4 oColor : COLOR0)
+{
+    oColor.rgb = iColor.rgb;
+
+#ifdef SIGNED_DISTANCE_FIELD
+    float distance = tex2D(sDiffMap, iTexCoord).a;
+    if (distance < 0.5f)
+        oColor.a = 0.0f;
+    else
+        oColor.a = iColor.a;
+
+    float width = 0.015f;
+    oColor.a *= smoothstep(0.5f - width, 0.5f + width, distance);
+#else
+    oColor.a = iColor.a * tex2D(sDiffMap, iTexCoord).a;
+#endif
+}
+
