9252d36abdcaac0268c9f3e95dd804f2312cf997
Lasse Öörni <loorni@gmail.com>
2014-08-30 13:41:54 +0300

    Clarify/fix header comments of the compression API.

--- Source/Engine/IO/Compression.h
+++ Source/Engine/IO/Compression.h
@@ -31,13 +31,13 @@ class Deserializer;
 class Serializer;
 class VectorBuffer;
 
-/// Estimate and return worst case LZ4 compressed output size for given input size.
+/// Estimate and return worst case LZ4 compressed output size in bytes for given input size.
 URHO3D_API unsigned EstimateCompressBound(unsigned srcSize);
 /// Compress data using the LZ4 algorithm and return the compressed data size. The needed destination buffer worst-case size is given by EstimateCompressBound().
 URHO3D_API unsigned CompressData(void* dest, const void* src, unsigned srcSize);
-/// Uncompress data using the LZ4 algorithm. Return the number of compressed data consumed.
+/// Uncompress data using the LZ4 algorithm. The uncompressed data size must be known. Return the number of compressed data bytes consumed.
 URHO3D_API unsigned DecompressData(void* dest, const void* src, unsigned destSize);
-/// Compress a source stream from current position to the destination stream using the LZ4 algorithm. Return true on success.
+/// Compress a source stream (from current position to the end) to the destination stream using the LZ4 algorithm. Return true on success.
 URHO3D_API bool CompressStream(Serializer& dest, Deserializer& src);
 /// Decompress a compressed source stream produced using CompressStream() to the destination stream. Return true on success.
 URHO3D_API bool DecompressStream(Serializer& dest, Deserializer& src);
