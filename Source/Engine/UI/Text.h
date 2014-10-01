2dc6c117cd98bf06e4e6aecaf304e91d2033b2ed
Lasse Öörni <loorni@gmail.com>
2014-10-01 18:18:08 +0300

    When a wordwrap text element exists in a parent layout, force a final layout update on the parent to prevent the parent remaining too large. Closes #475.

--- Source/Engine/UI/Text.h
+++ Source/Engine/UI/Text.h
@@ -170,7 +170,7 @@ protected:
     /// Filter implicit attributes in serialization process.
     virtual bool FilterImplicitAttributes(XMLElement& dest) const;
     /// Update text when text, font or spacing changed.
-    void UpdateText();
+    void UpdateText(bool onResize = false);
     /// Update cached character locations after text update, or when text alignment or indent has changed.
     void UpdateCharLocations();
     /// Validate text selection to be within the text.
