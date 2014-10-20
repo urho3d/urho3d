6099f0aafdc8de7ea67c7e79038735508d9998a5
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2014-10-20 18:43:49 +0800

    Fix FindDirect3D.cmake module to only pick June 2010 SDK. Closes #492.

--- Source/CMake/Modules/FindDirect3D.cmake
+++ Source/CMake/Modules/FindDirect3D.cmake
@@ -20,13 +20,11 @@
 # THE SOFTWARE.
 #
 
-# Find Microsoft DirectX installation
-# The search paths are based on realXtend Tundra CMake build system (https://github.com/realXtend/naali)
+# Find Microsoft DirectX installation (need June 2010 SDK or later)
 #
 #  DIRECT3D_FOUND
 #  DIRECT3D_INCLUDE_DIRS
 #  DIRECT3D_LIBRARIES
-#  DIRECT3D_COMPILER_LIBRARIES
 #
 
 if (NOT WIN32)
@@ -37,33 +35,34 @@ if (DIRECT3D_FOUND)
 endif ()
 
 set (DIRECTX_INC_SEARCH_PATH
-    "C:/Program Files (x86)/Microsoft DirectX SDK*/Include"
-    "C:/Program Files/Microsoft DirectX SDK*/Include"
+    "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/Include"
+    "C:/Program Files/Microsoft DirectX SDK (June 2010)/Include"
     "$ENV{DIRECTX_ROOT}/Include"
     "$ENV{DXSDK_DIR}/Include")
-find_path (DIRECT3D_INCLUDE_DIRS d3dcompiler.h ${DIRECTX_INC_SEARCH_PATH})
+find_path (DIRECT3D_INCLUDE_DIRS NAMES d3dcompiler.h PATHS ${DIRECTX_INC_SEARCH_PATH})
 
 if (CMAKE_CL_64)
     set (DIRECTX_LIB_SEARCH_PATH
-        "C:/Program Files (x86)/Microsoft DirectX SDK*/Lib/x64"
-        "C:/Program Files/Microsoft DirectX SDK*/Lib/x64"
+        "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/Lib/x64"
+        "C:/Program Files/Microsoft DirectX SDK (June 2010)/Lib/x64"
         "$ENV{DIRECTX_ROOT}/Lib/x64"
         "$ENV{DXSDK_DIR}/Lib/x64")
 else ()
     set (DIRECTX_LIB_SEARCH_PATH
-        "C:/Program Files (x86)/Microsoft DirectX SDK*/Lib"
-        "C:/Program Files (x86)/Microsoft DirectX SDK*/Lib/x86"
-        "C:/Program Files/Microsoft DirectX SDK*/Lib"
-        "C:/Program Files/Microsoft DirectX SDK*/Lib/x86"
+        "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/Lib"
+        "C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/Lib/x86"
+        "C:/Program Files/Microsoft DirectX SDK (June 2010)/Lib"
+        "C:/Program Files/Microsoft DirectX SDK (June 2010)/Lib/x86"
         "$ENV{DIRECTX_ROOT}/Lib"
         "$ENV{DIRECTX_ROOT}/Lib/x86"
         "$ENV{DXSDK_DIR}/Lib"
         "$ENV{DXSDK_DIR}/Lib/x86")
 endif ()
-find_library (DIRECT3D_LIBRARIES d3d9 ${DIRECTX_LIB_SEARCH_PATH})
-find_library (DIRECT3D_COMPILER_LIBRARIES d3dcompiler ${DIRECTX_LIB_SEARCH_PATH})
+find_library (DIRECT3D_LIB_D3D9 NAMES d3d9 PATHS ${DIRECTX_LIB_SEARCH_PATH})
+find_library (DIRECT3D_LIB_D3DCOMPILER NAMES d3dcompiler PATHS ${DIRECTX_LIB_SEARCH_PATH})
 
-if (DIRECT3D_INCLUDE_DIRS AND DIRECT3D_LIBRARIES AND DIRECT3D_COMPILER_LIBRARIES)
+if (DIRECT3D_INCLUDE_DIRS AND DIRECT3D_LIB_D3D9 AND DIRECT3D_LIB_D3DCOMPILER)
+    set (DIRECT3D_LIBRARIES ${DIRECT3D_LIB_D3D9} ${DIRECT3D_LIB_D3DCOMPILER})
     set (DIRECT3D_FOUND 1)
 endif ()
 
@@ -71,7 +70,8 @@ if (DIRECT3D_FOUND)
     include (FindPackageMessage)
     FIND_PACKAGE_MESSAGE (Direct3D "Found DirectX SDK: ${DIRECT3D_LIBRARIES} ${DIRECT3D_INCLUDE_DIRS}" "[${DIRECT3D_LIBRARIES}][${DIRECT3D_INCLUDE_DIRS}]")
 else ()
+    set (DIRECT3D_LIBRARIES d3d9 d3dcompiler)
     message (STATUS "DirectX SDK not found. This is not fatal if a recent Windows SDK is installed")
 endif ()
 
-mark_as_advanced (DIRECT3D_INCLUDE_DIRS DIRECT3D_LIBRARIES DIRECT3D_COMPILER_LIBRARIES)
+mark_as_advanced (DIRECT3D_INCLUDE_DIRS DIRECT3D_LIBRARIES)
