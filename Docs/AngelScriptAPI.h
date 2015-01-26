b8e66f96ac2c9680670c88a2ec37258d6adbfa58
urho3d-travis-ci <urho3d.travis.ci@gmail.com>
2015-01-26 20:36:19 +0000

    Travis CI: API documentation update at 2015-01-26 20:36:19 UTC. [ci package]

--- Docs/AngelScriptAPI.h
+++ Docs/AngelScriptAPI.h
@@ -1795,7 +1795,7 @@ void SetCustomTriangleMesh(CustomGeometry, const Vector3& = Vector3 ( 1 , 1 , 1 
 void SetCylinder(float, float, const Vector3& = Vector3 ( ), const Quaternion& = Quaternion ( ));
 void SetSphere(float, const Vector3& = Vector3 ( ), const Quaternion& = Quaternion ( ));
 void SetStaticPlane(const Vector3& = Vector3 ( ), const Quaternion& = Quaternion ( ));
-void SetTerrain();
+void SetTerrain(uint = 0);
 void SetTransform(const Vector3&, const Quaternion&);
 void SetTriangleMesh(Model, uint = 0, const Vector3& = Vector3 ( 1 , 1 , 1 ), const Vector3& = Vector3 ( ), const Quaternion& = Quaternion ( ));
 
