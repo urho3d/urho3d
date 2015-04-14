7df29b9897589b9a0f993901e1091570ac5c3fa3
marynate <mary.w.nate@gmail.com>
2015-04-14 19:48:31 +0800

    Add unsigned long and unsigned long long support to String += operator

--- Source/Urho3D/Container/Str.cpp
+++ Source/Urho3D/Container/Str.cpp
@@ -191,6 +191,16 @@ String& String::operator += (unsigned short rhs)
 {
     return *this += String(rhs);
 }
+    
+String& String::operator += (unsigned long rhs)
+{
+    return *this += String(rhs);
+}
+
+String& String::operator += (unsigned long long rhs)
+{
+	return *this += String(rhs);
+}
 
 String& String::operator += (float rhs)
 {
