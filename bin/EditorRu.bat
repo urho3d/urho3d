eb2457ce6a009416d2bb54cf391ce2c04e7e6688
Ivan K <to4ka.tut@bk.ru>
2015-07-21 23:12:20 +0400

    editor russification, add warnings to Localization::LoadJSON

--- bin/EditorRu.bat
+++ bin/EditorRu.bat
@@ -1,2 +1,5 @@
-Editor.bat -language ru -w -s
-
+@echo off
+if exist "%~dp0Urho3DPlayer.exe" (set "DEBUG=") else (set "DEBUG=_d")
+if exist "%~dp0..\share\Urho3D\Resources" (set "OPT1=-pp ..\share\Urho3D\Resources") else (set "OPT1=")
+if [%1] == [] (set "OPT2=-w -s") else (set "OPT2=")
+start "" "%~dp0Urho3DPlayer%DEBUG%" Scripts/Editor.as -language ru %OPT1% %OPT2% %*
