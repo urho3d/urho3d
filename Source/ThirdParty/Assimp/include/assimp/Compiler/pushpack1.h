07dab9946962a9d3c9906b628691f0d2abe50142
Lasse Öörni <loorni@gmail.com>
2015-09-15 14:29:53 +0300

    Update Assimp to latest head from github. Closes #786.

--- Source/ThirdParty/Assimp/include/assimp/Compiler/pushpack1.h
+++ Source/ThirdParty/Assimp/include/assimp/Compiler/pushpack1.h
@@ -18,9 +18,6 @@
 //
 // ===============================================================================
 
-// Modified by Yao Wei Tjong for Urho3D
-// Suppress 'gcc_struct' is being ignored warnings when compiling for Raspberry-PI
-
 #ifdef AI_PUSHPACK_IS_DEFINED
 #	error poppack1.h must be included after pushpack1.h
 #endif
@@ -29,7 +26,7 @@
 #	pragma pack(push,1)
 #	define PACK_STRUCT
 #elif defined( __GNUC__ )
-#	if defined(__clang__) || defined(RPI)
+#	if !defined(HOST_MINGW)
 #		define PACK_STRUCT	__attribute__((__packed__))
 #	else
 #		define PACK_STRUCT	__attribute__((gcc_struct, __packed__))
