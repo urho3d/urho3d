fc1f05f8dc956cb8b9d883daf76e0c471aaf0aea
Lasse Öörni <loorni@gmail.com>
2016-03-05 12:38:52 +0200

    Add script binding for SpriteSheet2D::SetTexture().

--- Source/Urho3D/AngelScript/Urho2DAPI.cpp
+++ Source/Urho3D/AngelScript/Urho2DAPI.cpp
@@ -78,6 +78,7 @@ static void RegisterSprite2D(asIScriptEngine* engine)
 static void RegisterSpriteSheet2D(asIScriptEngine* engine)
 {
     RegisterResource<SpriteSheet2D>(engine, "SpriteSheet2D");
+    engine->RegisterObjectMethod("SpriteSheet2D", "void set_texture(Texture2D@+)", asMETHOD(SpriteSheet2D, SetTexture), asCALL_THISCALL);
     engine->RegisterObjectMethod("SpriteSheet2D", "Texture2D@+ get_texture() const", asMETHOD(SpriteSheet2D, GetTexture), asCALL_THISCALL);
     engine->RegisterObjectMethod("SpriteSheet2D", "Sprite2D@+ GetSprite(const String&)", asMETHOD(SpriteSheet2D, GetSprite), asCALL_THISCALL);
     engine->RegisterObjectMethod("SpriteSheet2D", "void DefineSprite(const String&, const IntRect&, const Vector2& hotSpot=Vector2(0.5f, 0.5f), const IntVector2& offset = IntVector2::ZERO)", asMETHOD(SpriteSheet2D, DefineSprite), asCALL_THISCALL);
