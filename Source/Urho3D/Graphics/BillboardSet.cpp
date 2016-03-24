86ff45ce74251b03d2edea56691220a772275bae
damu <mail@gawag.org>
2016-03-24 12:07:05 +0100

    BillboardSet::UpdateVertexBuffer optimization: Now using a high precision version that's a bit slower but way more precise. Also fixed the missing function renames.

--- Source/Urho3D/Graphics/BillboardSet.cpp
+++ Source/Urho3D/Graphics/BillboardSet.cpp
@@ -511,36 +511,58 @@ void BillboardSet::UpdateBufferSize()
 
 void SinCosFFast(float x, float* sin, float* cos)
 {
-    if (x < -360.0f)    // wrap input angle to -360..360
-        x = fmodf(x,360);
-    else if (x > 360.0f)
-        x = fmodf(x,360);
+    x*=M_DEGTORAD;
+    if (x < -6.28318531f)    // wrap input angle to -360..360
+        x = fmodf(x,6.28318531f);
+    else if (x > 6.28318531f)
+        x = fmodf(x,6.28318531f);
 
-    if (x < -180.0f)    // wrap input angle to -180..180
-        x += 360.0f;
-    else if (x >  180.0f)
-        x -= 360.0f;
+    if (x < -3.14159265f)    // wrap input angle to -180..180
+        x += 6.28318531f;
+    else if (x > 3.14159265f)
+        x -= 6.28318531f;
 
+    float t;
     // compute sine and cosine: sin(x + 90°) = cos(x)
     if (x < 0)      // -180<=x<0
     {
-        *sin = 0.02222222222f * x + 0.0001234567901f * x * x;
+        t = 1.27323954f * x + 0.405284735f * x * x;
+        if (t < 0)
+            *sin = 0.225f * (t *-t - t) + t;
+        else
+            *sin = 0.225f * (t * t - t) + t;
 
-        x += 90.0f; // -90<=x<90
+        x += 1.57079632f; // -90<=x<90
     }
     else            // 0<=x<=180
     {
-        *sin = 0.02222222222f * x - 0.0001234567901f * x * x;
+        t = 1.27323954f * x - 0.405284735f * x * x;
+        if (t < 0)
+            *sin = 0.225f * (t *-t - t) + t;
+        else
+            *sin = 0.225f * (t * t - t) + t;
 
-        x += 90.0f; // 90<=x<=270
-        if (x >  180.0f)
-            x -= 360.0f;
+        x += 1.57079632f; // 90<=x<=270
+        if (x > 3.14159265f)
+            x -= 6.28318531f;
     }
 
     if (x < 0)
-        *cos = 0.02222222222f * x + 0.0001234567901f * x * x;
+    {
+        t = 1.27323954f * x + 0.405284735f * x * x;
+        if (t < 0)
+            *cos = 0.225f * (t *-t - t) + t;
+        else
+            *cos = 0.225f * (t * t - t) + t;
+    }
     else
-        *cos = 0.02222222222f * x - 0.0001234567901f * x * x;
+    {
+        t = 1.27323954f * x - 0.405284735f * x * x;
+        if (t < 0)
+            *cos = 0.225f * (t *-t - t) + t;
+        else
+            *cos = 0.225f * (t * t - t) + t;
+    }
 }
 
 void BillboardSet::UpdateVertexBuffer(const FrameInfo& frame)
@@ -616,7 +638,7 @@ void BillboardSet::UpdateVertexBuffer(const FrameInfo& frame)
             unsigned color = billboard.color_.ToUInt();
 
             float rotationMatrix[2][2];
-            sincosf_fast(billboard.rotation_ * M_DEGTORAD, &rotationMatrix[0][1], &rotationMatrix[0][0]);
+            SinCosFFast(billboard.rotation_, &rotationMatrix[0][1], &rotationMatrix[0][0]);
             rotationMatrix[1][0] = -rotationMatrix[0][1];
             rotationMatrix[1][1] = rotationMatrix[0][0];
 
@@ -669,7 +691,7 @@ void BillboardSet::UpdateVertexBuffer(const FrameInfo& frame)
             unsigned color = billboard.color_.ToUInt();
 
             float rot2D[2][2];
-            sincosf_fast(billboard.rotation_ * M_DEGTORAD, &rot2D[0][1], &rot2D[0][0]);
+            SinCosFFast(billboard.rotation_, &rot2D[0][1], &rot2D[0][0]);
             rot2D[1][0] = -rot2D[0][1];
             rot2D[1][1] = rot2D[0][0];
 
