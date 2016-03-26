191ffbc1d8b2858644d3c8d51eae1675bb402000
rokups <rokups@zoho.com>
2016-03-26 13:42:16 +0200

    Expose ValueAnimationInfo in shared library

--- Source/Urho3D/Scene/ValueAnimationInfo.h
+++ Source/Urho3D/Scene/ValueAnimationInfo.h
@@ -33,7 +33,7 @@ class ValueAnimation;
 struct VAnimEventFrame;
 
 /// Base class for a value animation instance, which includes animation runtime information and updates the target object's value automatically.
-class ValueAnimationInfo : public RefCounted
+class URHO3D_API ValueAnimationInfo : public RefCounted
 {
 public:
     /// Construct without target object.
