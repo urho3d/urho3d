1bcae0ca2b6109f06b32953b8b06aad0360371e1
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2016-04-30 12:42:10 +0800

    No-ops when VertexDeclaration.h is being included for OGL graphics API. You may want to uncomment the #error compiler directive instead to flag out the occurrences.

--- Source/Urho3D/Graphics/VertexDeclaration.h
+++ Source/Urho3D/Graphics/VertexDeclaration.h
@@ -22,7 +22,9 @@
 
 #pragma once
 
-#if defined(URHO3D_D3D11)
+#if defined(URHO3D_OPENGL)
+//#error OpenGL Graphics API does not have VertexDeclaration class, remove this header file in your build to fix this error
+#elif defined(URHO3D_D3D11)
 #include "Direct3D11/D3D11VertexDeclaration.h"
 #else
 #include "Direct3D9/D3D9VertexDeclaration.h"
