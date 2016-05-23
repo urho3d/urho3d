19b69aad7ed639745d518db17e016bfd8d3cde75
Bengt Soderstrom <bengtsoderstrom@Bengts-MBP.lan>
2016-05-23 20:42:09 +0200

    Text effect settings accessible through Text3D and minor variable name consistancy change in Text.h

--- Source/Urho3D/UI/Text.h
+++ Source/Urho3D/UI/Text.h
@@ -122,7 +122,7 @@ public:
     /// Set stroke thickness.
     void SetEffectStrokeThickness(int thickness);
     /// Set stroke rounding. Corners of the font will be rounded off in the stroke so the stroke won't have corners.
-    void SetEffectRoundStroke(bool roundCorners);
+    void SetEffectRoundStroke(bool roundStroke);
     /// Set effect color.
     void SetEffectColor(const Color& effectColor);
 
