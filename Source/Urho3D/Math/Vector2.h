84fa0ba9771893fd4c9bd37fc3bd021fba084908
Lasse Öörni <loorni@gmail.com>
2016-09-01 10:40:42 +0300

    Squashed commit of the following:
    commit 7cb949170f3585c860e9946dc4465fb73791e692
    Author: Henrik Heino <henrik.heino@gmail.com>
    Date:   Wed Aug 31 23:53:53 2016 +0300
    
        Suggested modifications to hash function.
    
    commit ca28aa5cb5ecf5cb746bbc78e417ee580327e676
    Author: Henrik Heino <henrik.heino@gmail.com>
    Date:   Tue Aug 30 02:39:42 2016 +0300
    
        IntVector2::ToHash()

--- Source/Urho3D/Math/Vector2.h
+++ Source/Urho3D/Math/Vector2.h
@@ -323,6 +323,9 @@ public:
     /// Return as string.
     String ToString() const;
 
+    /// Return hash value for HashSet & HashMap.
+    unsigned ToHash() const { return (unsigned)x_ * 31 + (unsigned)y_; }
+
     /// X coordinate.
     int x_;
     /// Y coordinate.
