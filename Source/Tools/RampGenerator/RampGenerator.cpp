13ed7bbbb6f4e0d1e7db6b13223835b86b582757
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2016-09-04 12:02:05 +0800

    Workaround a linking issue when using MinGW >= 6.1.0.

--- Source/Tools/RampGenerator/RampGenerator.cpp
+++ Source/Tools/RampGenerator/RampGenerator.cpp
@@ -28,10 +28,6 @@
 #include <windows.h>
 #endif
 
-
-#define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <STB/stb_image.h>
 #include <STB/stb_image_write.h>
 
 #include <Urho3D/DebugNew.h>
