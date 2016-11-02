6e7dfb7ba9c012007ea74967a4649076bd89f86a
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2016-11-03 01:13:51 +0800

    Fix for Wayland support for Linux build, native & cross-compiling mode.

--- CMake/Modules/FindWayland.cmake
+++ CMake/Modules/FindWayland.cmake
@@ -24,7 +24,10 @@
 #
 #  WAYLAND_FOUND
 #  WAYLAND_INCLUDE_DIRS
+#  WAYLAND_CORE_PROTOCOL_DIR
+#  WAYLAND_PROTOCOLS_DIR
 #  WAYLAND_CLIENT
+#  WAYLAND_SCANNER
 #  WAYLAND_CURSOR
 #  WAYLAND_EGL
 #  EGL
@@ -32,13 +35,16 @@
 #
 
 find_path (WAYLAND_INCLUDE_DIRS NAMES wayland-client.h DOC "Wayland include directory")
+find_path (WAYLAND_CORE_PROTOCOL_DIR NAMES wayland.xml PATHS /usr/share/wayland DOC "Wayland core protocol directory")
+find_path (WAYLAND_PROTOCOLS_DIR NAMES stable unstable PATHS /usr/share/wayland-protocols DOC "Wayland protocol directory")
 find_library (WAYLAND_CLIENT NAMES wayland-client DOC "Wayland client library")
+find_program (WAYLAND_SCANNER NAMES wayland-scanner DOC "Wayland scanner host tool")
 find_library (WAYLAND_CURSOR NAMES wayland-cursor DOC "Wayland compositor infrastructure - cursor library")
 find_library (WAYLAND_EGL NAMES wayland-egl DOC "Wayland EGL library")
 find_library (EGL NAMES EGL DOC "EGL library")
 find_library (XKB NAMES xkbcommon DOC "Xkb common library")
 
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (Wayland REQUIRED_VARS WAYLAND_CLIENT WAYLAND_CURSOR WAYLAND_EGL EGL XKB WAYLAND_INCLUDE_DIRS FAIL_MESSAGE "Could NOT find Wayland display server")
+find_package_handle_standard_args (Wayland REQUIRED_VARS WAYLAND_CLIENT WAYLAND_SCANNER WAYLAND_CURSOR WAYLAND_EGL EGL XKB WAYLAND_INCLUDE_DIRS WAYLAND_CORE_PROTOCOL_DIR WAYLAND_PROTOCOLS_DIR FAIL_MESSAGE "Could NOT find Wayland display server")
 
-mark_as_advanced (WAYLAND_INCLUDE_DIRS WAYLAND_CLIENT WAYLAND_CURSOR WAYLAND_EGL EGL XKB)
+mark_as_advanced (WAYLAND_INCLUDE_DIRS WAYLAND_CORE_PROTOCOL_DIR WAYLAND_PROTOCOLS_DIR WAYLAND_CLIENT WAYLAND_SCANNER WAYLAND_CURSOR WAYLAND_EGL EGL XKB)
