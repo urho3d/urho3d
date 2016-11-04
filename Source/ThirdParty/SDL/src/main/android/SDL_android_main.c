ad97bfacd08cc6ba5957d49b39fe4f1595193261
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2016-11-05 02:38:07 +0800

    Fix Android build.

--- Source/ThirdParty/SDL/src/main/android/SDL_android_main.c
+++ Source/ThirdParty/SDL/src/main/android/SDL_android_main.c
@@ -22,7 +22,7 @@
 extern void SDL_Android_Init(JNIEnv* env, jclass cls, jstring filesDir);
 
 /* This prototype is needed to prevent a warning about the missing prototype for global function below */
-JNIEXPORT int JNICALL Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jclass cls, jobject array);
+JNIEXPORT int JNICALL Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jclass cls, jobject array, jstring filesDir);
 
 /* Start up the SDL app */
 JNIEXPORT int JNICALL Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jclass cls, jobject array, jstring filesDir)
