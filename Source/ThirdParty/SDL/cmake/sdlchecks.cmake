628e4b27959cf40d9a16af47e91334912303bffd
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2016-11-17 01:20:47 +0800

    Quick fix for #1681 on Linux build, requires ibus-dev(el) package.

--- Source/ThirdParty/SDL/cmake/sdlchecks.cmake
+++ Source/ThirdParty/SDL/cmake/sdlchecks.cmake
@@ -697,7 +697,7 @@ macro(CheckVivante)
   if(VIDEO_VIVANTE)
     # Urho3D - bug fix - when cross-compiling the headers are rooted, either use "--sysroot" compiler flag or use CMAKE_REQUIRED_INCLUDES (e.g. on RPI) to cater for it
     set (CMAKE_REQUIRED_INCLUDES_VIVANTE_SAVED ${CMAKE_REQUIRED_INCLUDES})
-    if (CMAKE_CROSSCOMPILING AND NOT "${CMAKE_C_FLAGS} ${CMAKE_REQUIRED_FLAGS}" MATCHES sysroot)
+    if (CMAKE_CROSSCOMPILING AND NOT "${CMAKE_C_FLAGS} ${CMAKE_REQUIRED_FLAGS}" MATCHES --sysroot)
       find_path (VIVANTE_INCLUDE_DIRS NAMES gc_vdk.h EGL/eglvivante.h)
       if (VIVANTE_INCLUDE_DIRS)
         # Assume the header search path has not been adjusted elsewhere yet, there is no harm anyway when a same entry is added twice into the list
