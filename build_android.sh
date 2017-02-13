2c145049dfbca91c618d0b2ca6696158e860a113
Brian Bosak <brielcccc@gmail.com>
2017-02-13 10:12:09 -0600

    Fixed build script.

--- build_android.sh
+++ build_android.sh
@@ -1,14 +1,13 @@
 for abi in "armeabi" "armeabi-v7a" "arm64-v8a" "mips" "mips64";
 do
 mkdir -p libs
-mkdir -p build
+mkdir -p build-$abi
 export ANDROID_ABI=$abi
-./cmake_android.sh build -DURHO3D_LIB_TYPE=SHARED -DANDROID_ABI=${ANDROID_ABI} -DURHO3D_ANGELSCRIPT=0
-cd build
+./cmake_android.sh build-$abi -DURHO3D_LIB_TYPE=SHARED -DANDROID_ABI=${ANDROID_ABI} -DURHO3D_ANGELSCRIPT=0
+cd build-$abi
 make -j32
 cd libs
 cp -ar . ../../libs/
 cd ../../
-rm -rf build
 done
 
