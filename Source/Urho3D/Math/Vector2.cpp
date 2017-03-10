ca76683568639e36a687b0385a68ee5aadf350b9
Rokas Kupstys <rokups@zoho.com>
2017-03-10 18:22:29 +0200

    IntVector2 constants

--- Source/Urho3D/Math/Vector2.cpp
+++ Source/Urho3D/Math/Vector2.cpp
@@ -39,6 +39,11 @@ const Vector2 Vector2::DOWN(0.0f, -1.0f);
 const Vector2 Vector2::ONE(1.0f, 1.0f);
 
 const IntVector2 IntVector2::ZERO;
+const IntVector2 IntVector2::LEFT(-1, 0);
+const IntVector2 IntVector2::RIGHT(1, 0);
+const IntVector2 IntVector2::UP(0, 1);
+const IntVector2 IntVector2::DOWN(0, -1);
+const IntVector2 IntVector2::ONE(1, 1);
 
 String Vector2::ToString() const
 {
