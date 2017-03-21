d2911ad4550af13222a76a3d292a3aad00dedcf6
TheComet <alex.murray@gmx.ch>
2017-03-21 15:54:42 +0100

    Adding ik library to Urho3D thirdparty libs

---
+++ Source/ThirdParty/ik/ik/include/ik/backtrace.h
@@ -0,0 +1,22 @@
+#ifndef LIGHTSHIP_UTIL_BACKTRACE_H
+#define LIGHTSHIP_UTIL_BACKTRACE_H
+
+#define BACKTRACE_SIZE 64
+
+#include "ik/gen/config.h"
+#include "ik/pstdint.h"
+
+C_HEADER_BEGIN
+
+/*!
+ * @brief Generates a backtrace.
+ * @param[in] size The maximum number of frames to walk.
+ * @return Returns an array of char* arrays.
+ * @note The returned array must be freed manually with FREE(returned_array).
+ */
+IK_PUBLIC_API char**
+get_backtrace(int* size);
+
+C_HEADER_END
+
+#endif /* LIGHTSHIP_UTIL_BACKTRACE_H */
