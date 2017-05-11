1b8c3e67695a807c92d8d0745d9411af295de2a4
meshonline <artgolf1000@gmail.com>
2017-05-11 21:35:35 +0800

    add seek function

--- Source/Urho3D/Audio/BufferedSoundStream.h
+++ Source/Urho3D/Audio/BufferedSoundStream.h
@@ -40,6 +40,9 @@ public:
     /// Destruct.
     ~BufferedSoundStream();
 
+    /// Seek to sample number
+    virtual int Seek(unsigned int sample_number);
+    
     /// Produce sound data into destination. Return number of bytes produced. Called by SoundSource from the mixing thread.
     virtual unsigned GetData(signed char* dest, unsigned numBytes);
 
