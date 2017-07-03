urho3d/urho3d

commit 39930c9ab4923b0d0f652934e99c9650afce39e4

Fix bug in FontFaceFreeType::CanLoadAllGlyphs that can make text vanish
Closes #1976

FontFaceFreeType checks at runtime whether the whole font can be loaded
into a single texture, by preflighting all the necessary texture atlas
placements in CanLoadAllGlyphs.

However, this preflight operation was slightly different from the real
texture atlas generation, so sometimes CanLoadAllGlyphs was incorrectly
returning true. The LoadCharGlyph would then switch to a new texture
atlas (via SetupNextTexture). This would usually make Text elements
vanish, as the rendered glyphs were in the wrong texture.

We fix this bug with two changes:
- Make CanLoadAllGlyphs process the same char codes in the same order
- Make CanLoadAllGlyphs skip zero-size glyphs (as LoadCharGlyph does)

We also log an error if SetupNextTexture is called unexpectedly, to
help catch future occurrences.

Source/Urho3D/UI/FontFaceFreeType.cpp
@@ -77,7 +77,8 @@ private:
 FontFaceFreeType::FontFaceFreeType(Font* font) :
     FontFace(font),
     face_(0),
-    loadMode_(FT_LOAD_DEFAULT)
+    loadMode_(FT_LOAD_DEFAULT),
+    hasMutableGlyph_(false)
 {
 }
 
@@ -140,16 +141,18 @@ bool FontFaceFreeType::Load(const unsigned char* fontData, unsigned fontDataSize
     unsigned numGlyphs = (unsigned)face->num_glyphs;
     URHO3D_LOGDEBUGF("Font face %s (%dpt) has %d glyphs", GetFileName(font_->GetName()).CString(), pointSize, numGlyphs);
 
-    PODVector<unsigned> charCodes(numGlyphs);
-    for (unsigned i = 0; i < numGlyphs; ++i)
-        charCodes[i] = 0;
+    PODVector<unsigned> charCodes(numGlyphs + 1, 0);
+
+    // Attempt to load space glyph first regardless if it's listed or not
+    // In some fonts (Consola) it is missing
+    charCodes[0] = 32;
 
     FT_UInt glyphIndex;
     FT_ULong charCode = FT_Get_First_Char(face, &glyphIndex);
     while (glyphIndex != 0)
     {
         if (glyphIndex < numGlyphs)
-            charCodes[glyphIndex] = (unsigned)charCode;
+            charCodes[glyphIndex + 1] = (unsigned)charCode;
 
         charCode = FT_Get_Next_Char(face, charCode, &glyphIndex);
     }
@@ -175,7 +178,7 @@ bool FontFaceFreeType::Load(const unsigned char* fontData, unsigned fontDataSize
 
     int textureWidth = maxTextureSize;
     int textureHeight = maxTextureSize;
-    bool loadAllGlyphs = CanLoadAllGlyphs(charCodes, textureWidth, textureHeight);
+    hasMutableGlyph_ = !CanLoadAllGlyphs(charCodes, textureWidth, textureHeight);
 
     SharedPtr<Image> image(new Image(font_->GetContext()));
     image->SetSize(textureWidth, textureHeight, 1);
@@ -183,17 +186,13 @@ bool FontFaceFreeType::Load(const unsigned char* fontData, unsigned fontDataSize
     memset(imageData, 0, (size_t)(image->GetWidth() * image->GetHeight()));
     allocator_.Reset(FONT_TEXTURE_MIN_SIZE, FONT_TEXTURE_MIN_SIZE, textureWidth, textureHeight);
 
-    // Attempt to load space glyph first regardless if it's listed or not
-    // In some fonts (Consola) it is missing
-    LoadCharGlyph(32, image);
-
-    for (unsigned i = 0; i < numGlyphs; ++i)
+    for (unsigned i = 0; i < charCodes.Size(); ++i)
     {
         unsigned charCode = charCodes[i];
         if (charCode == 0)
             continue;
 
-        if (!loadAllGlyphs && (charCode > 0xff))
+        if (hasMutableGlyph_ && (charCode > 0xff))
             break;
 
         if (!LoadCharGlyph(charCode, image))
@@ -276,14 +275,11 @@ bool FontFaceFreeType::Load(const unsigned char* fontData, unsigned fontDataSize
             URHO3D_LOGWARNING("Can not read kerning information: not version 0");
     }
 
-    if (loadAllGlyphs)
+    if (!hasMutableGlyph_)
     {
         FT_Done_Face(face);
         face_ = 0;
-        hasMutableGlyph_ = false;
     }
-    else
-        hasMutableGlyph_ = true;
 
     return true;
 }
@@ -330,9 +326,12 @@ bool FontFaceFreeType::CanLoadAllGlyphs(const PODVector<unsigned>& charCodes, in
         {
             int width = Max(RoundToPixels(slot->metrics.width), (int)slot->bitmap.width);
             int height = Max(RoundToPixels(slot->metrics.height), (int)slot->bitmap.rows);
-            int x, y;
-            if (!allocator.Allocate(width + 1, height + 1, x, y))
-                return false;
+            if (width > 0 && height > 0)
+            {
+                int x, y;
+                if (!allocator.Allocate(width + 1, height + 1, x, y))
+                    return false;
+            }
         }
     }
 
@@ -384,6 +383,12 @@ bool FontFaceFreeType::LoadCharGlyph(unsigned charCode, Image* image)
             int x, y;
             if (!allocator_.Allocate(fontGlyph.width_ + 1, fontGlyph.height_ + 1, x, y))
             {
+                if (!HasMutableGlyphs())
+                {
+                    // Should never happen if CanLoadAllGlyphs was used correctly
+                    URHO3D_LOGERROR("Unexpectedly ran out of space in LoadCharGlyph");
+                }
+
                 if (!SetupNextTexture(allocator_.GetWidth(), allocator_.GetHeight()))
                     return false;
 

commit 2ab978e9683deadb2c8e27d3baa09593ec50cecd

Fix kerning pairs (#2003)
Kerning values were being associated with the wrong char codes.
This was broken in commit 39930c9a, which moved some code around
to ensure that the space character is always the first one loaded.

Source/Urho3D/UI/FontFaceFreeType.cpp
@@ -268,8 +268,8 @@ bool FontFaceFreeType::Load(const unsigned char* fontData, unsigned fontDataSize
                         unsigned rightIndex = deserializer.ReadUShort();
                         short amount = FixedToFloat(deserializer.ReadShort());
 
-                        unsigned leftCharCode = leftIndex < numGlyphs ? charCodes[leftIndex] : 0;
-                        unsigned rightCharCode = rightIndex < numGlyphs ? charCodes[rightIndex] : 0;
+                        unsigned leftCharCode = leftIndex < numGlyphs ? charCodes[leftIndex + 1] : 0;
+                        unsigned rightCharCode = rightIndex < numGlyphs ? charCodes[rightIndex + 1] : 0;
                         if (leftCharCode != 0 && rightCharCode != 0)
                         {
                             unsigned value = (leftCharCode << 16) + rightCharCode;