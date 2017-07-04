826b548a49d680a9e8b1f1058ce9ce36b6162e5d
Konstantin Guschin <gkostik1@gmail.com>
2017-07-04 13:36:29 +0700

    Add csv and base64 encodings for tmxfile

--- Source/Urho3D/Core/StringUtils.cpp
+++ Source/Urho3D/Core/StringUtils.cpp
@@ -31,6 +31,11 @@
 namespace Urho3D
 {
 
+static const String base64_chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789+/";
+
 unsigned CountElements(const char* buffer, char separator)
 {
     if (!buffer)
@@ -751,6 +756,51 @@ String GetFileSizeString(unsigned long long memorySize)
     }
 
     return output;
+}
+
+static inline bool isBase64(char c) {
+    return (isalnum(c) || (c == '+') || (c == '/'));
+}
+
+PODVector<unsigned char> DecodeBase64(String encoded_string) {
+    int in_len = encoded_string.Length();
+    int i = 0;
+    int j = 0;
+    int in_ = 0;
+    unsigned char char_array_4[4], char_array_3[3];
+    PODVector<unsigned char> ret;
+
+    while (in_len-- && (encoded_string[in_] != '=') && isBase64(encoded_string[in_])) {
+        char_array_4[i++] = encoded_string[in_]; in_++;
+        if (i == 4) {
+            for (i = 0; i <4; i++)
+                char_array_4[i] = base64_chars.Find(char_array_4[i]);
+
+            char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+            char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+            char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+            for (i = 0; (i < 3); i++)
+                ret.Push(char_array_3[i]);
+            i = 0;
+        }
+    }
+
+    if (i) {
+        for (j = i; j <4; j++)
+            char_array_4[j] = 0;
+
+        for (j = 0; j <4; j++)
+            char_array_4[j] = base64_chars.Find(char_array_4[j]);
+
+        char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+        char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+        char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+        for (j = 0; (j < i - 1); j++) ret.Push(char_array_3[j]);
+    }
+
+    return ret;
 }
 
 }
