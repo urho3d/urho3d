ddf64ccdb8da0e76a14b744bfa16525257ba7c09
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2017-07-30 19:27:05 +0800

    Make the symlinked batch files launchable via Windows Explorer. Fix #2046.

--- bin/PBRDemoDeferred.bat
+++ bin/PBRDemoDeferred.bat
@@ -1,3 +1,6 @@
 @echo off
-if exist "%~dp0Urho3DPlayer.exe" (set "DEBUG=") else (set "DEBUG=_d")
-"%~dp0Urho3DPlayer%DEBUG%" Scripts/42_PBRMaterials.as -renderpath CoreData/RenderPaths/PBRDeferred.xml %*
+setlocal
+set "dirname=%~dp0"
+if %0 == "%~dpnx0" where /q "%cd%:%~nx0" && set "dirname=%cd%\"
+if exist "%dirname%Urho3DPlayer.exe" (set "DEBUG=") else (set "DEBUG=_d")
+"%dirname%Urho3DPlayer%DEBUG%" Scripts/42_PBRMaterials.as -renderpath CoreData/RenderPaths/PBRDeferred.xml %*
