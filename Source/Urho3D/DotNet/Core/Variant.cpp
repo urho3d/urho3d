4dff58334f443504a446d22b5bb40a446aa8cfa8
1vanK <1vanK@users.noreply.github.com>
2017-11-15 08:58:20 +0300

    DotNet bindings: fix get Variant from VariantMap

--- Source/Urho3D/DotNet/Core/Variant.cpp
+++ Source/Urho3D/DotNet/Core/Variant.cpp
@@ -5,24 +5,14 @@ using namespace Urho3D;
 extern "C"
 {
 
-// Warning when "Variant VariantMap_GetValue(const VariantMap& nativeInstance, StringHash key)
-URHO3D_API void VariantMap_GetValue(const VariantMap& nativeInstance, StringHash key, Variant* value)
+URHO3D_API void VariantMap_GetValue(VariantMap& nativeInstance, StringHash key, Variant& value)
 {
-    (*value) = nativeInstance[key];
+    value = nativeInstance[key];
 }
 
-URHO3D_API void VariantMap_SetValue(VariantMap* nativeInstance, StringHash key, Variant* value)
+URHO3D_API void VariantMap_SetValue(VariantMap& nativeInstance, StringHash key, Variant& value)
 {
-    printf("VariantMap_SetValue key=%u value.type=%i value.value=%i\n", key, value->GetType(), value->GetInt());
-
-//    Variant testValue = 17;
-
-    unsigned char bytes[sizeof(Variant)];
-    memcpy(bytes, value, sizeof(Variant));
-    for (int i = 0; i < sizeof(Variant); i++)
-        printf("%i = %i\n", i, bytes[i]);
-
-    (*nativeInstance)[key] = (*value);
+    nativeInstance[key] = value;
 }
 
 }
