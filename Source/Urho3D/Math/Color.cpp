3d7e8aba0228355b067ddd291c61ff15dbbb1869
Aban Hussain <hsnabn@gmail.com>
2017-11-22 02:24:54 +0500

    Add FromUInt function to Color.

--- Source/Urho3D/Math/Color.cpp
+++ Source/Urho3D/Math/Color.cpp
@@ -64,6 +64,14 @@ Vector3 Color::ToHSV() const
     return Vector3(h, s, v);
 }
 
+void Color::FromUInt(unsigned color)
+{
+    a_ = (float)(((color >> 24) & 0xff) / 255);
+    b_ = (float)(((color >> 16) & 0xff) / 255);
+    g_ = (float)(((color >> 8)  & 0xff) / 255);
+    r_ = (float)(((color >> 0)  & 0xff) / 255);
+}
+
 void Color::FromHSL(float h, float s, float l, float a)
 {
     float c;
