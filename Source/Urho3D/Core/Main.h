9eed60fc73ba7cdda3974a78213e999d359fe91e
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2018-03-17 16:43:20 +0800

    Keep the SDL_main symbol around to fix the Android Static build config. Fix #2267 [ci only: Android, OSX] [skip appveyor]

--- Source/Urho3D/Core/Main.h
+++ Source/Urho3D/Core/Main.h
@@ -70,8 +70,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, in
 // Android or iOS or tvOS: use SDL_main
 #elif defined(__ANDROID__) || defined(IOS) || defined(TVOS)
 #define URHO3D_DEFINE_MAIN(function) \
-extern "C" int SDL_main(int argc, char** argv); \
-int URHO3D_API SDL_main(int argc, char** argv) \
+extern "C" __attribute__((visibility("default"))) int SDL_main(int argc, char** argv); \
+int SDL_main(int argc, char** argv) \
 { \
     Urho3D::ParseArguments(argc, argv); \
     return function; \
