0626fdbdf98c885a04c0d9593478e8bb1d6fc2c9
Ricardo Abreu <ricab@ricabhome.org>
2018-03-25 19:56:27 +0100

    fix memory leak in SpritePacker tool - fixes CID 63638

--- Source/Tools/SpritePacker/SpritePacker.cpp
+++ Source/Tools/SpritePacker/SpritePacker.cpp
@@ -328,7 +328,8 @@ void Run(Vector<String>& arguments)
     spriteSheetImage.SetSize(packedWidth, packedHeight, 4);
 
     // zero out image
-    spriteSheetImage.SetData((unsigned char*)calloc(sizeof(unsigned char), (size_t)packedWidth * packedHeight * 4));
+    // Ugly: depends on implementation details - is this really needed?
+    memset(spriteSheetImage.GetData(), 0, packedWidth * packedHeight * 4);
 
     XMLFile xml(context);
     XMLElement root = xml.CreateRoot("TextureAtlas");
