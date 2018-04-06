42d70200736bbd5da04db2d30bd06fb696067924
Enhex <enhex0@gmail.com>
2018-04-07 01:16:25 +0300

    use nullptr instead of 0

--- Source/Urho3D/UI/Selectable.cpp
+++ Source/Urho3D/UI/Selectable.cpp
@@ -23,7 +23,7 @@ void Selectable::GetBatches(PODVector<UIBatch>& batches, PODVector<float>& verte
     if ((hovering_ && hoverColor_.a_ > 0.0) || (selected_ && selectionColor_.a_ > 0.0f))
     {
         bool both = hovering_ && selected_ && hoverColor_.a_ > 0.0 && selectionColor_.a_ > 0.0f;
-        UIBatch batch(this, BLEND_ALPHA, currentScissor, 0, &vertexData);
+        UIBatch batch(this, BLEND_ALPHA, currentScissor, nullptr, &vertexData);
         batch.SetColor(both ? selectionColor_.Lerp(hoverColor_, 0.5f) :
             (selected_ && selectionColor_.a_ > 0.0f ? selectionColor_ : hoverColor_));
         batch.AddQuad(0.f, 0.f, (float)GetWidth(), (float)GetHeight(), 0, 0);
