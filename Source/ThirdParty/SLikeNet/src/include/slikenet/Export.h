94b97707b3195a2e7cfe9bf2401f0f650a19e6a0
Arnis Lielturkss <arnis.lielturks@gmail.com>
2018-04-15 18:09:44 +0300

    MinGW shared fix

--- Source/ThirdParty/SLikeNet/src/include/slikenet/Export.h
+++ Source/ThirdParty/SLikeNet/src/include/slikenet/Export.h
@@ -15,7 +15,7 @@
 
 #include "defines.h"
 
-#if defined(_WIN32) && !(defined(__GNUC__)  || defined(__GCCXML__)) && !defined(_RAKNET_LIB) && defined(_RAKNET_DLL)
+#if (defined(_WIN32) || defined(__MINGW32__)) && !(defined(__GNUC__)  || defined(__GCCXML__)) && !defined(_RAKNET_LIB) && defined(_RAKNET_DLL)
 #define RAK_DLL_EXPORT __declspec(dllexport)
 #else
 #define RAK_DLL_EXPORT  
