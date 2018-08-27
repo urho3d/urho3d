79d755399c53f66eeb80648f62e1b2cd0330a73f
Florastamine <florastamine@gmail.com>
2018-08-27 21:31:53 +0700

    Also support querying adapter on D3D9.

--- Source/Urho3D/Graphics/Direct3D9/D3D9Graphics.cpp
+++ Source/Urho3D/Graphics/Direct3D9/D3D9Graphics.cpp
@@ -479,6 +479,11 @@ bool Graphics::SetMode(int width, int height, bool fullscreen, bool borderless, 
     impl_->device_->Present(nullptr, nullptr, nullptr, nullptr);
 
 #ifdef URHO3D_LOGGING
+    D3DADAPTER_IDENTIFIER9 id = {0};
+    HRESULT hr = impl_->interface_->GetAdapterIdentifier(D3DADAPTER_DEFAULT, 0, &id);
+    if (S_OK == hr)
+      URHO3D_LOGINFOF("Adapter used %s", id.Description);
+
     String msg;
     msg.AppendWithFormat("Set screen mode %dx%d %s monitor %d", width_, height_, (fullscreen_ ? "fullscreen" : "windowed"), monitor_);
     if (borderless_)
