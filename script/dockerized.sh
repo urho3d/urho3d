71ed76ece04c1dd2028e9938ad7b4c5910f26b82
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2018-10-19 20:13:43 +0800

    Fallback workaround on older Docker CLI version.

--- script/dockerized.sh
+++ script/dockerized.sh
@@ -38,8 +38,15 @@ if [[ $(docker version -f {{.Client.Version}}) =~ ^([0-9]+)\.0*([0-9]+)\. ]] && 
         --name dockerized$BuildEnvironment \
         urho3d/dockerized$BuildEnvironment $@
 else
-    echo "Error: Docker CLI version is too old, minimum required version is 18.09.x"
-    exit 2;
+    # Fallback workaround on older Docker CLI version
+    docker run -it --rm -h fishtank \
+        -e HOST_UID=$(id -u) -e HOST_GID=$(id -g) \
+        --env-file <(perl -ne 'chomp; print "$_=$ENV{$_}\n" if defined $ENV{$_}' $PROJECT_DIR/.env-file) \
+        --mount type=bind,source=$PROJECT_DIR,target=/project_dir \
+        --mount source=ccache_dir,target=/ccache_dir \
+        --mount source=home_dir,target=/home/urho3d \
+        --name dockerized$BuildEnvironment \
+        urho3d/dockerized$BuildEnvironment $@
 fi
 
 # vi: set ts=4 sw=4 expandtab:
