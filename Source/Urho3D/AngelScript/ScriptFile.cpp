1f4ff5754229fae26b9a2b09a68192a0531ef0c7
Yao Wei Tjong 姚伟忠 <weitjong@gmail.com>
2019-09-22 11:00:35 +0800

    Minor clean up on spacing.

--- Source/Urho3D/AngelScript/ScriptFile.cpp
+++ Source/Urho3D/AngelScript/ScriptFile.cpp
@@ -861,10 +861,10 @@ void ScriptFile::SetParameters(asIScriptContext* context, asIScriptFunction* fun
             context->SetArgDWord(i, (asDWORD)parameters[i].GetInt());
             break;
 
-		case asTYPEID_INT64:
-		case asTYPEID_UINT64:
-			context->SetArgQWord(i, (asQWORD)parameters[i].GetInt64());
-			break;
+        case asTYPEID_INT64:
+        case asTYPEID_UINT64:
+            context->SetArgQWord(i, (asQWORD)parameters[i].GetInt64());
+            break;
 
         case asTYPEID_FLOAT:
             context->SetArgFloat(i, parameters[i].GetFloat());
