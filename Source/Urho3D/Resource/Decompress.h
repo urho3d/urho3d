caf2a741435196ade33e04dc63bfebe711432603
songruining <songruining@acmoba.net>
2019-10-21 11:24:41 +0800

    Use ETCPACK as 3rd-party to decompress ETC1/ETC2 texture, and remove old ETC1 decompression code.

--- Source/Urho3D/Resource/Decompress.h
+++ Source/Urho3D/Resource/Decompress.h
@@ -30,10 +30,8 @@ namespace Urho3D
 /// Decompress a DXT compressed image to RGBA.
 URHO3D_API void
     DecompressImageDXT(unsigned char* rgba, const void* blocks, int width, int height, int depth, CompressedFormat format);
-/// Decompress an ETC1 compressed image to RGBA.
-URHO3D_API void DecompressImageETC(unsigned char* rgba, const void* blocks, int width, int height);
-/// Decompress an ETC2 compressed image to RGBA.
-URHO3D_API void DecompressImageETC2(unsigned char* dstImage, const void* blocks, int width, int height, bool hasAlpha);
+/// Decompress an ETC1/ETC2 compressed image to RGBA.
+URHO3D_API void DecompressImageETC(unsigned char* dstImage, const void* blocks, int width, int height, bool hasAlpha);
 /// Decompress a PVRTC compressed image to RGBA.
 URHO3D_API void DecompressImagePVRTC(unsigned char* rgba, const void* blocks, int width, int height, CompressedFormat format);
 /// Flip a compressed block vertically.
