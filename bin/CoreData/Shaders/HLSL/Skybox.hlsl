d382d96d93ab1e49cd1ba6c64d60a174b43fddce
Gleb Lebedev <gleb@gleblebedev.com>
2020-07-01 23:49:48 +0100

    PR cleanup

--- bin/CoreData/Shaders/HLSL/Skybox.hlsl
+++ bin/CoreData/Shaders/HLSL/Skybox.hlsl
@@ -10,8 +10,7 @@ void VS(float4 iPos : POSITION,
     out float4 oPos : OUTPOSITION)
 {
 #ifdef IGNORENODETRANSFORM
-    float4x3 m = cViewInv;
-    float4x3 modelMatrix = { float3(1,0,0), float3(0,1,0), float3(0,0,1), m[3] };
+    float4x3 modelMatrix = { float3(1.0, 0.0, 0.0), float3(0.0, 1.0, 0.0), float3(0.0, 0.0, 1.0), cViewInv[3] };
 #else
     float4x3 modelMatrix = iModelMatrix;
 #endif
