0e4e3843cdd7ee29e17d86e9a4736f128be0e77e
Anton Katekov <void0main@gmail.com>
2020-07-16 02:29:43 +0900

    Implement state machine

--- Source/Urho3D/Graphics/StateMachine.cpp
+++ Source/Urho3D/Graphics/StateMachine.cpp
@@ -16,30 +16,156 @@
 #include "../DebugNew.h"
 
 
+
 namespace Urho3D
 {
 
-StateMachine::StateMachine(Context* context)
+StateMachineState::StateMachineState(const String &name)
+:name_(name)
+{}
+
+StateMachineState::~StateMachineState()
+{
+    for (auto i = transitions_.Begin(); i != transitions_.End(); i++)
+    {
+        delete i->second_;
+    }
+    transitions_.Clear();
+}
+
+bool StateMachineState::AddTransition(const String &stateTo, const String &transitionName)
+{
+    if (transitions_.Contains(transitionName))
+    {
+        return false;
+    }
+    
+    StateMachineTransition *transition = new StateMachineTransition(transitionName, name_, stateTo);
+    transitions_.Insert(Pair<String, StateMachineTransition *>(transitionName, transition));
+    return true;
+}
+
+bool StateMachineState::CanTransit(const String &transitionName)
+{
+    return transitions_.Contains(transitionName);
+}
+
+
+
+StateMachineConfig::StateMachineConfig(Context* context)
 :ResourceWithMetadata(context)
 {}
 
-StateMachine::~StateMachine()
+StateMachineConfig::~StateMachineConfig()
 {
-
+    for (auto i = states_.Begin(); i != states_.End(); i++)
+    {
+        delete i->second_;
+    }
+    states_.Clear();
 }
+
+void StateMachineConfig::RegisterObject(Context* context)
+{
+    context->RegisterFactory<StateMachineConfig>();
+}
+
+bool StateMachineConfig::BeginLoad(Deserializer& source)
+{
+    return true;
+}
+
+bool StateMachineConfig::Save(Serializer& dest) const
+{
+    return true;
+}
+
+bool StateMachineConfig::AddState(const String &stateName)
+{
+    if (states_.Contains(stateName))
+    {
+        return false;
+    }
+    
+    StateMachineState *newState = new StateMachineState(stateName);
+    states_.Insert(Pair<String, StateMachineState *>(stateName, newState));
+    return true;
+}
+
+bool StateMachineConfig::AddTransition(const String &stateFrom, const String &stateTo, const String &transitionName)
+{
+    auto stateIterator = states_.Find(stateFrom);
+    if (stateIterator == states_.End())
+    {
+        return false;
+    }
+    
+    if (!states_.Contains(stateTo))
+    {
+        return false;
+    }
+    
+    StateMachineState *state = states_[stateFrom];
+    return state->AddTransition(stateTo, transitionName);
+}
+
+//bool StateMachineConfig::IsTransitionMuted(const String &transitionName)
+//{}
+//
+//bool StateMachineConfig::SetTransitionMuted(const String &transitionName, bool value)
+//{}
+
+bool StateMachineConfig::CanTransitFromState(const String &stateName, const String &transitionName)
+{
+    auto stateIterator = states_.Find(stateName);
+    if (stateIterator == states_.End())
+    {
+        return false;
+    }
+    
+    StateMachineState *state = stateIterator->second_;
+    return state->CanTransit(transitionName);
+}
+
+
+
+StateMachine::StateMachine(StateMachineConfig *config, const String &initialState)
+:config_(config)
+,stateCurrent_(config->states_[initialState])
+{
+    
+}
+
+StateMachine::~StateMachine() = default;
 
 void StateMachine::RegisterObject(Context* context)
 {
     context->RegisterFactory<StateMachine>();
 }
 
-bool StateMachine::BeginLoad(Deserializer& source) 
+void StateMachine::SetDelegate(StateMachineDelegate *delegate)
 {
-    return true;
+    delegate_ = delegate;
 }
 
-bool StateMachine::Save(Serializer& dest) const 
+StateMachineDelegate *StateMachine::GetDelegate()
 {
+    return delegate_;
+}
+
+bool StateMachine::Transit(const String &transitionName)
+{
+    if (!stateCurrent_->CanTransit(transitionName))
+    {
+        return false;
+    }
+    
+    String oldStateName = stateCurrent_->name_;
+    stateCurrent_ = config_->states_[stateCurrent_->transitions_[transitionName]->stateTo_];
+    if (delegate_)
+    {
+        delegate_->StateMachineDidTransit(this, oldStateName, transitionName, stateCurrent_->name_);
+    }
     return true;
 }
 
--- Source/Urho3D/Graphics/StateMachine.h
+++ Source/Urho3D/Graphics/StateMachine.h
@@ -27,16 +27,68 @@
 namespace Urho3D
 {
 
-/// State machine component.
-class URHO3D_API StateMachine : public ResourceWithMetadata
-{
-    URHO3D_OBJECT(StateMachine, ResourceWithMetadata);
+class StateMachine;
 
+struct URHO3D_API StateMachineTransition
+{
+    /// Transition name
+    String name_;
+    
+    /// initial state
+    String stateFrom_;
+    /// destination state
+    String stateTo_;
+    
+//    /// disables a transition (unity style)
+//    bool mute_ = false;
+
+    /// Construct.
+    StateMachineTransition(const String &name, const String &stateFrom, const String &stateTo)
+    :name_(name)
+    ,stateFrom_(stateFrom)
+    ,stateTo_(stateTo)
+    {
+    }
+
+};
+
+struct URHO3D_API StateMachineState
+{
+    // State name
+    String name_;
+    
+    /// All transitions from this state
+    /// key represents trigger
+    HashMap<String, StateMachineTransition *> transitions_;
+    
+    /// Construct.
+    StateMachineState(const String &name);
+    ~StateMachineState();
+    
+    bool AddTransition(const String &stateTo, const String &transitionName);
+    bool CanTransit(const String &transitionName);
+
+};
+
+class URHO3D_API StateMachineDelegate 
+{
+public:
+    virtual void StateMachineDidTransit(StateMachine *sender, const String &stateFrom, const String &transitionName, const String &stateTo) = 0;
+
+};
+
+/// State machine resource.
+class URHO3D_API StateMachineConfig : public ResourceWithMetadata
+{
+    URHO3D_OBJECT(StateMachineConfig, ResourceWithMetadata);
+    
+    friend class StateMachine;
+    
 public:
     /// Construct.
-    explicit StateMachine(Context* context);
+    explicit StateMachineConfig(Context* context);
     /// Destruct.
-    ~StateMachine() override;
+    ~StateMachineConfig() override;
     /// Register object factory. Drawable must be registered first.
     static void RegisterObject(Context* context);
 
@@ -45,6 +97,55 @@ public:
     /// Save resource. Return true if successful.
     bool Save(Serializer& dest) const override;
 
+    /// Create new state
+    bool AddState(const String &stateName);
+    /// Create new transition
+    bool AddTransition(const String &stateFrom, const String &stateTo, const String &transitionName);
+    
+//    bool IsTransitionMuted(const String &transitionName);
+//    bool SetTransitionMuted(const String &transitionName, bool value);
+    
+    /// Verify if transition is correct
+    bool CanTransitFromState(const String &stateName, const String &transitionName);
+
+private:
+    
+    /// Available states
+    HashMap<String, StateMachineState *> states_;
+};
+
+
+
+/// State machine user
+class URHO3D_API StateMachine: public RefCounted
+{
+    
+public:
+    /// Construct.
+    explicit StateMachine(StateMachineConfig *config, const String &initialState);
+    /// Destruct.
+    ~StateMachine() override;
+    /// Register object factory. Drawable must be registered first.
+    static void RegisterObject(Context* context);
+    
+    /// Set delegate to listen events. Overrides existing delegate
+    void SetDelegate(StateMachineDelegate *delegate);
+    /// Get existing delegate
+    StateMachineDelegate *GetDelegate();
+    
+    /// Execute transition
+    bool Transit(const String &transitionName);
+    
+private:
+    
+    /// State machine configuration
+    SharedPtr<StateMachineConfig> config_ = nullptr;
+    
+    /// Delegate for listening for transitions
+    StateMachineDelegate *delegate_ = nullptr;
+    
+    /// Actual current state
+    StateMachineState *stateCurrent_ = nullptr;
 };
 
 }
