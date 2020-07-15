d1b1ddb4a0b8d69dd281254203b39a37dfabeb14
Anton Katekov <void0main@gmail.com>
2020-07-15 23:42:54 +0900

    Create files

---
+++ Source/Urho3D/Graphics/StateMachine.cpp
@@ -0,0 +1,17 @@
+#include "StateMachine.h"
+
+
+
+namespace Urho3D
+{
+
+StateMachine::StateMachine(Context* context)
+:Component(context)
+{}
+
+StateMachine::~StateMachine() = default;
+
+void StateMachine::RegisterObject(StateMachine* context)
+{}
+
+}
