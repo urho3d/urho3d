9e9b8e7566b8d271de40936b1afad1fad5b0810a
Anton Katekov <void0main@gmail.com>
2020-07-21 18:24:09 +0900

    Create unit testing target

---
+++ Source/Tests/testTest.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "testBase.h"
+#include <Urho3D/Math/Vector2.h>
+#include <Urho3D/Math/Vector3.h>
+#include <Urho3D/Math/Ray.h>
+
+
+TEST_F(UrhoBaseTest, firstTest)
+{
+    
+    int a = 10;
+    ASSERT_EQ(a, 10);
+}
