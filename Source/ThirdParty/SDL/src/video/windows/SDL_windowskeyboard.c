9d9717832535bc1c7850823264c6e130f2c5bb09
wws <1274704958@qq.com>
2020-10-27 12:22:24 +0800

    Fix Chinese input problems on Windows and Android platform

--- Source/ThirdParty/SDL/src/video/windows/SDL_windowskeyboard.c
+++ Source/ThirdParty/SDL/src/video/windows/SDL_windowskeyboard.c
@@ -881,9 +881,9 @@ IME_HandleMessage(HWND hwnd, UINT msg, WPARAM wParam, LPARAM *lParam, SDL_VideoD
     case WM_INPUTLANGCHANGE:
         IME_InputLangChanged(videodata);
         break;
-    case WM_IME_SETCONTEXT:
-        *lParam = 0;
-        break;
+    //case WM_IME_SETCONTEXT:
+    //    *lParam = 0;
+    //    break;
     case WM_IME_STARTCOMPOSITION:
         trap = SDL_TRUE;
         break;
