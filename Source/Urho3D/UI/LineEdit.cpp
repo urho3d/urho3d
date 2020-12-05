ee9a9402518d4a80948977b663ab66dc608ff04e
1vanK <1vanK@users.noreply.github.com>
2020-12-05 23:52:32 +0300

    Stylistic changes

--- Source/Urho3D/UI/LineEdit.cpp
+++ Source/Urho3D/UI/LineEdit.cpp
@@ -566,15 +566,17 @@ void LineEdit::UpdateText()
 {
     unsigned utf8Length = line_.LengthUTF8();
 
-    // if maxLength_ nonzero, truncate line to enforce length
-    if (maxLength_ > 0 && utf8Length > maxLength_)
+    // If maxLength_ nonzero, truncate line to enforce length
+    if (maxLength_ && utf8Length > maxLength_)
     {
-      line_ = line_.SubstringUTF8(0, maxLength_);
-      utf8Length = maxLength_;
+        line_ = line_.SubstringUTF8(0, maxLength_);
+        utf8Length = maxLength_;
     }
 
     if (!echoCharacter_)
+    {
         text_->SetText(line_);
+    }
     else
     {
         String echoText;
@@ -582,6 +584,7 @@ void LineEdit::UpdateText()
             echoText.AppendUTF8(echoCharacter_);
         text_->SetText(echoText);
     }
+
     if (cursorPosition_ > utf8Length)
     {
         cursorPosition_ = utf8Length;
