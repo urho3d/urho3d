c4082368d7315c7a658cade35b4d0094500762f5
1vanK <1vanK@users.noreply.github.com>
2021-01-20 23:40:55 +0300

    Update boost::preprocessor to 1.75.0

--- Source/ThirdParty/boost/preprocessor/tuple/replace.hpp
+++ Source/ThirdParty/boost/preprocessor/tuple/replace.hpp
@@ -13,9 +13,6 @@
 # define BOOST_PREPROCESSOR_TUPLE_REPLACE_HPP
 #
 # include <boost/preprocessor/config/config.hpp>
-#
-# if BOOST_PP_VARIADICS
-#
 # include <boost/preprocessor/array/replace.hpp>
 # include <boost/preprocessor/array/to_tuple.hpp>
 # include <boost/preprocessor/tuple/to_array.hpp>
@@ -23,15 +20,13 @@
 # /* BOOST_PP_TUPLE_REPLACE */
 #
 # define BOOST_PP_TUPLE_REPLACE(tuple, i, elem) \
-	BOOST_PP_ARRAY_TO_TUPLE(BOOST_PP_ARRAY_REPLACE(BOOST_PP_TUPLE_TO_ARRAY(tuple), i, elem)) \
+    BOOST_PP_ARRAY_TO_TUPLE(BOOST_PP_ARRAY_REPLACE(BOOST_PP_TUPLE_TO_ARRAY(tuple), i, elem)) \
 /**/
 #
 # /* BOOST_PP_TUPLE_REPLACE_D */
 #
 # define BOOST_PP_TUPLE_REPLACE_D(d, tuple, i, elem) \
-	BOOST_PP_ARRAY_TO_TUPLE(BOOST_PP_ARRAY_REPLACE_D(d, BOOST_PP_TUPLE_TO_ARRAY(tuple), i, elem)) \
+    BOOST_PP_ARRAY_TO_TUPLE(BOOST_PP_ARRAY_REPLACE_D(d, BOOST_PP_TUPLE_TO_ARRAY(tuple), i, elem)) \
 /**/
-#
-# endif // BOOST_PP_VARIADICS
 #
 # endif // BOOST_PREPROCESSOR_TUPLE_REPLACE_HPP
