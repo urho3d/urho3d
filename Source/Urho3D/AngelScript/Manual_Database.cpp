a2c4314ce2a50e224b55803450d849b7fbb3d94a
1vanK <1vanK@users.noreply.github.com>
2021-04-08 11:27:29 +0300

    Update Manual_Database.cpp

--- Source/Urho3D/AngelScript/Manual_Database.cpp
+++ Source/Urho3D/AngelScript/Manual_Database.cpp
@@ -94,10 +94,7 @@ static DbConnection* DbConnection_DbConnection_Context()
 
 static void RegisterDbConnection(asIScriptEngine* engine)
 {
-    MemberCollection members;
-    CollectMembers_Object(members);
     RegisterMembers_Object<DbConnection>(engine, "DbConnection");
-    RegisterMembers(engine, "DbConnection", members);
 
     //engine->RegisterObjectBehaviour("DbConnection", asBEHAVE_FACTORY, "DbConnection@+ f()", AS_FUNCTION(DbConnection_DbConnection_Context), AS_CALL_CDECL);
 
