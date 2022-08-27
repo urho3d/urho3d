29b90e381ed959214c212d82a888efaf1b11a8e4
1vanK <1vanK@users.noreply.github.com>
2022-08-27 23:45:50 +0300

    MemoryBuffer: unsigned char to u8

--- Source/Urho3D/IO/MemoryBuffer.h
+++ Source/Urho3D/IO/MemoryBuffer.h
@@ -18,9 +18,9 @@ public:
     /// Construct as read-only with a pointer and size.
     MemoryBuffer(const void* data, unsigned size);
     /// Construct from a vector, which must not go out of scope before MemoryBuffer.
-    explicit MemoryBuffer(Vector<unsigned char>& data);
+    explicit MemoryBuffer(Vector<u8>& data);
     /// Construct from a read-only vector, which must not go out of scope before MemoryBuffer.
-    explicit MemoryBuffer(const Vector<unsigned char>& data);
+    explicit MemoryBuffer(const Vector<u8>& data);
 
     /// Read bytes from the memory area. Return number of bytes actually read.
     unsigned Read(void* dest, unsigned size) override;
@@ -30,14 +30,14 @@ public:
     i32 Write(const void* data, i32 size) override;
 
     /// Return memory area.
-    unsigned char* GetData() { return buffer_; }
+    u8* GetData() { return buffer_; }
 
     /// Return whether buffer is read-only.
     bool IsReadOnly() { return readOnly_; }
 
 private:
     /// Pointer to the memory area.
-    unsigned char* buffer_;
+    u8* buffer_;
     /// Read-only flag.
     bool readOnly_;
 };
