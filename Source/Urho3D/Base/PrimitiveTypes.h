f58cf294a675b6fd32e1d34f711b913c5ef8b225
1vanK <1vanK@users.noreply.github.com>
2022-10-18 12:09:56 +0300

    PrimitiveTypes: do not use std::uint64_t and etc for underlying types (#3120)

--- Source/Urho3D/Base/PrimitiveTypes.h
+++ Source/Urho3D/Base/PrimitiveTypes.h
@@ -4,21 +4,20 @@
 #pragma once
 
 #include <cstddef> // std::byte
-#include <cstdint> // std::int8_t, ...
 
 // User can inject Urho3D::PrimitiveTypes into other namespace
 namespace Urho3D::PrimitiveTypes
 {
 
-// https://en.cppreference.com/w/cpp/types/integer
-using i8 = std::int8_t;
-using u8 = std::uint8_t;
-using i16 = std::int16_t;
-using u16 = std::uint16_t;
-using i32 = std::int32_t;
-using u32 = std::uint32_t;
-using i64 = std::int64_t;
-using u64 = std::uint64_t;
+// https://en.cppreference.com/w/cpp/language/types
+using i8 = signed char;
+using u8 = unsigned char;
+using i16 = short;
+using u16 = unsigned short;
+using i32 = int;
+using u32 = unsigned;
+using i64 = long long;
+using u64 = unsigned long long;
 
 // Unicode code point (UTF-32 code unit)
 using c32 = char32_t;
