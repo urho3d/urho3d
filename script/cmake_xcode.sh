4c2f80fbb8cd01c7df9188251c1728c5017eff5b
Nathanial Lydick <SirNate0@gmail.com>
2022-12-06 09:18:07 -0500

    Do not force XCode to use the old build system. Allow CMake to choose for us.

--- script/cmake_xcode.sh
+++ script/cmake_xcode.sh
@@ -3,6 +3,6 @@
 # Copyright (c) 2008-2022 the Urho3D project
 # License: MIT
 
-$(dirname $0)/cmake_generic.sh "$@" -G Xcode -T buildsystem=1
+$(dirname $0)/cmake_generic.sh "$@" -G Xcode
 
 # vi: set ts=4 sw=4 expandtab:
